05524ef49240f01ced6aa0c76fcc398156f368de
Andre Pimenta <andrepimenta7@gmail.com>
2022-09-06 16:30:00 +0100

    Update MetaMaskHooks.ts

--- packages/sdk-react/src/MetaMaskHooks.ts
+++ packages/sdk-react/src/MetaMaskHooks.ts
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   Chain,
   useClient,
   useConnect as useConnectWagmi,
   useNetwork,
 } from 'wagmi';
 
+// eslint-disable-next-line import/export
 export * from 'wagmi';
+
+// eslint-disable-next-line import/export
 export const useConnect = () => {
   const client = useClient();
   const connector = client.connectors[0];
@@ -48,8 +53,10 @@ export const useSwitchOrAddNetwork = () => {
     } catch (switchError) {
       // This error code indicates that the chain has not been added to MetaMask.
       if (
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         switchError.code === 4902 ||
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         switchError?.data?.originalError?.code === 4902
       ) {
@@ -85,10 +92,12 @@ export const useSwitchOrAddNetwork = () => {
             params: [params],
           });
         } catch (addError) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
           setError(addError);
         }
       } else {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         setError(switchError);
       }
