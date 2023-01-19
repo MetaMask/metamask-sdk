82e59a6a9faafce39a66fc63b2f61628deb8842b
Arthur Breton <arthur.breton@consensys.net>
2023-01-19 09:53:44 +0800

    feat(dev-react): ui

--- packages/devreact/src/App.tsx
+++ packages/devreact/src/App.tsx
@@ -1,14 +1,268 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { MetaMaskSDK } from '@metamask/sdk';
+import { ChannelConfig, CommunicationLayerPreference, ConnectionStatus, KeyInfo } from '@metamask/sdk-communication-layer';
 
-function App() {
+const sdk = new MetaMaskSDK({
+  useDeeplink: false,
+  communicationLayerPreference: CommunicationLayerPreference.SOCKET,
+  communicationServerUrl: 'http://localhost:5400',
+  enableDebug: true,
+  ecies: {
+    enabled:true,
+    debug:true,
+  }
+});
+
+
+export const App = () => {
+  const [channelConfig, setChannelConfig] = useState<ChannelConfig>();
+  const [keyInfo, setKeyInfo] = useState<KeyInfo>();
+  const [chain, setChain] = useState("");
+  const [account, setAccount] = useState("");
+  const [response, setResponse] = useState<unknown>("");
+  const [connected, setConnected] = useState(false);
+  const [connectionStatus, setConnectionStatus] = useState(ConnectionStatus.DISCONNECTED);
+
+  const connect = () => {
+    if(!window.ethereum) {
+      throw new Error(`invalid ethereum provider`);
+    }
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+        params: [],
+      })
+      .then((res) => console.log("request accounts", res))
+      .catch((e) => console.log("request accounts ERR", e));
+  };
+
+
+  const addEthereumChain = () => {
+    if(!window.ethereum) {
+      throw new Error(`invalid ethereum provider`);
+    }
+
+    window.ethereum
+      .request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x89",
+            chainName: "Polygon",
+            blockExplorerUrls: ["https://polygonscan.com"],
+            nativeCurrency: { symbol: "MATIC", decimals: 18 },
+            rpcUrls: ["https://polygon-rpc.com/"],
+          },
+        ],
+      })
+      .then((res) => console.log("add", res))
+      .catch((e) => console.log("ADD ERR", e));
+  };
+
+  useEffect(() => {
+    console.debug(`useEffect`, window.ethereum);
+
+    window.ethereum?.on("chainChanged", (chain) => {
+      console.log(chain);
+      setChain(chain as string);
+    });
+    window.ethereum?.on("accountsChanged", (accounts) => {
+      console.log(accounts);
+      setAccount((accounts as string[])?.[0]);
+    });
+    window.ethereum?.on('connect', (connectInfo) => {
+      console.log(`connected`, connectInfo);
+      setConnected(true);
+      setChannelConfig(sdk.getChannelConfig());
+      setKeyInfo(sdk.getKeyInfo());
+    })
+    window.ethereum?.on("disconnect", (error) => {
+      console.log('disconnected', error);
+      setConnected(false);
+    });
+  }, []);
+
+  const sendTransaction = async () => {
+    const to = "0x0000000000000000000000000000000000000000";
+    const transactionParameters = {
+      to, // Required except during contract publications.
+      from: window.ethereum?.selectedAddress, // must match user's active address.
+      value: "0x5AF3107A4000", // Only required to send ether to the recipient from the initiating external account.
+    };
+
+    try {
+      // txHash is a hex string
+      // As with any RPC call, it may throw an error
+      const txHash = await window.ethereum?.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      }) as string;
+
+      setResponse(txHash);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const sign = async () => {
+    const msgParams = JSON.stringify({
+      domain: {
+        // Defining the chain aka Rinkeby testnet or Ethereum Main Net
+        chainId: parseInt(window.ethereum?.chainId ?? "", 16),
+        // Give a user friendly name to the specific contract you are signing for.
+        name: "Ether Mail",
+        // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
+        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+        // Just let's you know the latest version. Definitely make sure the field name is correct.
+        version: "1",
+      },
+
+      // Defining the message signing data content.
+      message: {
+        /*
+         - Anything you want. Just a JSON Blob that encodes the data you want to send
+         - No required fields
+         - This is DApp Specific
+         - Be as explicit as possible when building out the message schema.
+        */
+        contents: "Hello, Bob!",
+        attachedMoneyInEth: 4.2,
+        from: {
+          name: "Cow",
+          wallets: [
+            "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+            "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF",
+          ],
+        },
+        to: [
+          {
+            name: "Bob",
+            wallets: [
+              "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+              "0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57",
+              "0xB0B0b0b0b0b0B000000000000000000000000000",
+            ],
+          },
+        ],
+      },
+      // Refers to the keys of the *types* object below.
+      primaryType: "Mail",
+      types: {
+        // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        // Not an EIP712Domain definition
+        Group: [
+          { name: "name", type: "string" },
+          { name: "members", type: "Person[]" },
+        ],
+        // Refer to PrimaryType
+        Mail: [
+          { name: "from", type: "Person" },
+          { name: "to", type: "Person[]" },
+          { name: "contents", type: "string" },
+        ],
+        // Not an EIP712Domain definition
+        Person: [
+          { name: "name", type: "string" },
+          { name: "wallets", type: "address[]" },
+        ],
+      },
+    });
+
+    var from = window.ethereum?.selectedAddress;
+
+    var params = [from, msgParams];
+    var method = "eth_signTypedData_v4";
+
+    try {
+      const resp = await window.ethereum?.request({ method, params });
+      setResponse(resp);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const disconnect = () => {
+    sdk.disconnect();
+  }
+
+  const reset = () => {
+    console.debug(`init reset`);
+    sdk.resetKeys();
+  }
+
   return (
     <div className="App">
-      <button onClick={() => { console.debug(`TODO`) }}>Debug</button>
+      <div className="sdkConfig">
+        <>
+          <p>ChannelId: {channelConfig?.channelId}</p>
+          <p>{`Expiration: ${channelConfig?.validUntil ?? ''}`}</p>
+          <div>
+            <div className='row'>
+              <div className='rowLabel'>DAPP Private Key:</div>
+              <div className='rowValue even'>{keyInfo?.encryptionKey}</div>
+            </div>
+            <div className='row'>
+              <div className='rowLabel'>DAPP Public Key:</div>
+              <div className='rowValue odd'>{keyInfo?.decryptionKey}</div>
+            </div>
+            <div className='row'>
+              <div className='rowLabel'>MM Private Key:</div>
+              <div className='rowValue even'>{keyInfo?.otherPubKey}</div>
+            </div>
+          </div>
+        </>
+      </div>
+      <>
+        <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+          {connected ? "Connected" : "Connect"}
+        </button>
+
+        <button style={{ padding: 10, margin: 10 }} onClick={sign} disabled={!connected}>
+          Sign
+        </button>
+
+        <button style={{ padding: 10, margin: 10 }} onClick={sendTransaction}  disabled={!connected}>
+          Send transaction
+        </button>
+
+        <button style={{ padding: 10, margin: 10 }} onClick={addEthereumChain}  disabled={!connected}>
+          Add ethereum chain
+        </button>
+
+        {connected &&
+          <>
+            <button style={{ padding: 10, margin: 10, backgroundColor: 'red' }} onClick={disconnect}  disabled={!connected}>
+              Disconnect
+            </button>
+            <button style={{ padding: 10, margin: 10, backgroundColor: 'red' }} onClick={reset}  disabled={!connected}>
+              Reset
+            </button>
+          </>
+        }
+
+        <div>
+          {connected ?
+            <>
+              {chain && `Connected chain: ${chain}`}
+              <p></p>
+              {account && `Connected account: ${account}`}
+              <p></p>
+              {response && `Last request response: ${response}`}
+            </> : <>
+              __DISCONNECTED__
+            </>
+          }
+        </div>
+
+        </>
     </div>
   );
 }
-
-export default App;
