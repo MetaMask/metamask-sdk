b957d9268d023a194334cd3811807af314afc3b1
Arthur Breton <arthur.breton@consensys.net>
2023-02-01 18:46:28 +0800

    feat: add default export for sdk

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,3 +3,4 @@ import { MetaMaskSDK, MetaMaskSDKOptions } from './sdk';
 export * from '@metamask/sdk-communication-layer';
 export type { MetaMaskSDKOptions };
 export { MetaMaskSDK };
+export default MetaMaskSDK;
