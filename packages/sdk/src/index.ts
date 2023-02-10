8bda1502c66891634e0434c5a4fe20db3e06ad23
Arthur Breton <arthur.breton@consensys.net>
2023-02-10 16:09:53 +0800

    feat: export logging options type

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,6 +1,7 @@
 import { MetaMaskSDK, MetaMaskSDKOptions } from './sdk';
+import type { SDKLoggingOptions } from './types/SDKLoggingOptions';
 
 export * from '@metamask/sdk-communication-layer';
-export type { MetaMaskSDKOptions };
+export type { MetaMaskSDKOptions, SDKLoggingOptions };
 export { MetaMaskSDK };
 export default MetaMaskSDK;
