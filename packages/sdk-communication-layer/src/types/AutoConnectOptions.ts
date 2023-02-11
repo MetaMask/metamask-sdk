015d856285ee3e3937cf124b2cefbb7a7509244e
Arthur Breton <arthur.breton@consensys.net>
2023-02-11 20:44:37 +0800

    feat: add configuration option for auto connect

--- packages/sdk-communication-layer/src/types/AutoConnectOptions.ts
+++ packages/sdk-communication-layer/src/types/AutoConnectOptions.ts
@@ -1,6 +1,6 @@
 export interface AutoConnectOptions {
-  enable: boolean;
-  timeout: number; // number is ms, how long to wait for mm mobile
+  enable?: boolean;
+  timeout?: number; // number is ms, how long to wait for mm mobile
   // type: behavior once timeout is reached
   // RENEW -> generate new channel id
   // LINK -> call deeplink
