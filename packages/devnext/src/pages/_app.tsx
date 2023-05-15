119def7d7934d0c5f177feef99fb97512d656f80
abretonc7s <arthur.breton@consensys.net>
2023-05-15 21:17:32 +0800

    feat: setup initial devnextjs project

---
+++ packages/devnext/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-unassigned-import
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
