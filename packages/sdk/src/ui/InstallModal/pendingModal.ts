e7f9a050ab7085fc4acfadf6ffe9dc65bb66dc95
abretonc7s <arthur.breton@consensys.net>
2023-05-16 11:23:55 +0800

    feat: more deps update

--- packages/sdk/src/ui/InstallModal/pendingModal.ts
+++ packages/sdk/src/ui/InstallModal/pendingModal.ts
@@ -3,8 +3,8 @@ import PendingModal from './pendingModal-web';
 //#elif _NODEJS
 import PendingModal from './pendingModal-nodejs';
 //#else */
-// import PendingModal from './pendingModal-nodejs';
-import PendingModal from './pendingModal-web';
+import PendingModal from './pendingModal-nodejs';
+// import PendingModal from './pendingModal-web';
 // #endif
 
 export default PendingModal;
