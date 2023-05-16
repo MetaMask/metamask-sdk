1e115c23670dc071e8418fd82620b12e13e7ce41
abretonc7s <arthur.breton@consensys.net>
2023-05-16 16:51:54 +0800

    feat: setup tailwindcss on nextjs

--- packages/devnext/src/pages/_app.tsx
+++ packages/devnext/src/pages/_app.tsx
@@ -1,7 +1,28 @@
 // eslint-disable-next-line import/no-unassigned-import
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { MetaMaskProvider } from '@metamask/sdk-react';
+import { useEffect, useState } from 'react';
+import { MetaMaskSDKOptions } from '@metamask/sdk';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [sdkOptions, setSDKOptions] = useState<MetaMaskSDKOptions>({ autoConnect: { enable: false } });
+
+  useEffect(() => {
+    setSDKOptions({
+      communicationServerUrl: 'http://192.168.50.114:4000',
+      logging: {
+        developerMode: false,
+      },
+      autoConnect: {
+        enable: true,
+      },
+      dappMetadata: {
+        name: 'DevNext',
+        url: 'http://devnext.fakeurl.com',
+      }
+    })
+  }, []);
+
+  return (<MetaMaskProvider sdkOptions={sdkOptions}><Component {...pageProps} /></MetaMaskProvider>);
 }
