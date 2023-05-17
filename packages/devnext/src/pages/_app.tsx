96151186550d487aed20d915d11444114072cb61
abretonc7s <arthur.breton@consensys.net>
2023-05-17 11:12:57 +0800

    feat: enable plaintext on nextjs sdk-react dev test

--- packages/devnext/src/pages/_app.tsx
+++ packages/devnext/src/pages/_app.tsx
@@ -1,28 +1,51 @@
 // eslint-disable-next-line import/no-unassigned-import
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { MetaMaskProvider } from '@metamask/sdk-react';
+import { createClient, MetaMaskProvider, WagmiConfig } from '@metamask/sdk-react';
 import { useEffect, useState } from 'react';
 import { MetaMaskSDKOptions } from '@metamask/sdk';
+import { getDefaultProvider } from 'ethers'
+
+const serverClient = createClient({
+  autoConnect: true,
+  provider: getDefaultProvider(),
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [sdkOptions, setSDKOptions] = useState<MetaMaskSDKOptions>({ autoConnect: { enable: false } });
+  const [sdkOptions, setSDKOptions] = useState<MetaMaskSDKOptions>({ autoInit: false });
+  const [clientSide, setClientSide] = useState(false);
 
   useEffect(() => {
+    console.debug(`INIT NOW`);
     setSDKOptions({
       communicationServerUrl: 'http://192.168.50.114:4000',
       logging: {
-        developerMode: false,
+        developerMode: true,
+        plaintext: true,
       },
+      autoInit: true,
       autoConnect: {
         enable: true,
+      },
+      storage: {
+        enabled: true,
       },
       dappMetadata: {
         name: 'DevNext',
         url: 'http://devnext.fakeurl.com',
       }
     })
+    setClientSide(true);
   }, []);
 
-  return (<MetaMaskProvider sdkOptions={sdkOptions}><Component {...pageProps} /></MetaMaskProvider>);
+  return (
+    <>
+      {
+        clientSide ?
+          <MetaMaskProvider sdkOptions={sdkOptions}><Component {...pageProps} /></MetaMaskProvider> :
+          (
+            <WagmiConfig client={serverClient}><Component {...pageProps} /></WagmiConfig>
+          )
+      }
+    </>);
 }
