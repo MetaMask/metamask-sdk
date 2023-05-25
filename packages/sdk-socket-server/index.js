33e41aaf8e0e221b38bbf146a9b666302a2ac8b3
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-05-26 00:27:14 +0200

    moves from express to fastify, adds cache to userid hash, moves to batch events per interval vs number, improves socket.io code

--- packages/sdk-socket-server/index.js
+++ packages/sdk-socket-server/index.js
@@ -2,25 +2,28 @@
 require('dotenv').config();
 const crypto = require('crypto');
 const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
+const fastify = require('fastify')({ logger: true });
+const LRU = require('lru-cache');
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
-const app = express();
+const userIdHashCache = new LRU({
+  max: 5000,
+  maxAge: 1000 * 60 * 60 * 24,
+});
 
-const server = http.createServer(app);
+const server = http.createServer(fastify);
 const { Server } = require('socket.io');
 const { RateLimiterMemory } = require('rate-limiter-flexible');
 
 const rateLimiter = new RateLimiterMemory({
-  points: 5, // 5 points
-  duration: 1, // per second
+  points: 5,
+  duration: 1,
 });
 
 const rateLimiterMesssage = new RateLimiterMemory({
-  points: 50, // 5 points
-  duration: 1, // per second
+  points: 50,
+  duration: 1,
 });
 
 const io = new Server(server, {
@@ -30,14 +33,14 @@ const io = new Server(server, {
 });
 const cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-app.options('*', cors());
+fastify.register(require('fastify-cors'), {
+  origin: '*',
+});
+
+fastify.register(require('fastify-helmet'));
+fastify.register(require('fastify-formbody'));
 
 const uuid = require('uuid');
-
-const helmet = require('helmet');
 
 const Analytics = require('analytics-node');
 
@@ -48,7 +51,7 @@ const analytics = new Analytics(
     ? process.env.SEGMENT_API_KEY_DEBUG
     : process.env.SEGMENT_API_KEY_PRODUCTION,
   {
-    flushAt: isDevelopment ? 1 : 20,
+    flushInterval: isDevelopment ? 1000 : 10000,
     errorHandler: (err) => {
       console.error('Analytics-node flush failed.');
       console.error(err);
@@ -56,81 +59,60 @@ const analytics = new Analytics(
   },
 );
 
-app.use(helmet());
-app.disable('x-powered-by');
-
-app.get('/', (_req, res) => {
-  res.json({ success: true });
+fastify.get('/', async (_request, reply) => {
+  reply.code(200).send({ success: true });
 });
 
-// flushes all Segment events when Node process is interrupted for any reason
-// https://segment.com/docs/connections/sources/catalog/libraries/server/node/#long-running-process
-const exitGracefully = async (code) => {
-  console.log('Flushing events');
-  await analytics.flush(function (err) {
-    console.log('Flushed, and now this program can exit!');
-
-    if (err) {
-      console.log(err);
-    }
-    // eslint-disable-next-line node/no-process-exit
-    process.exit(code);
-  });
-};
-
-[
-  'beforeExit',
-  'uncaughtException',
-  'unhandledRejection',
-  'SIGHUP',
-  'SIGINT',
-  'SIGQUIT',
-  'SIGILL',
-  'SIGTRAP',
-  'SIGABRT',
-  'SIGBUS',
-  'SIGFPE',
-  'SIGUSR1',
-  'SIGSEGV',
-  'SIGUSR2',
-  'SIGTERM',
-].forEach((evt) => process.on(evt, exitGracefully));
-
-app.post('/debug', (_req, res) => {
+fastify.post('/debug', async (request, reply) => {
   try {
-    const { body } = _req;
+    const { body } = request;
 
     if (!body.event) {
-      return res.status(400).json({ error: 'event is required' });
+      return reply.status(400).send({ error: 'event is required' });
     }
 
     const id = body.id || 'socket.io-server';
-    const userIdHash = crypto.createHash('sha1').update(id).digest('hex');
+    let userIdHash = userIdHashCache.get(id);
 
-    analytics.track(
-      {
-        userId: userIdHash,
-        event: body.event,
-        ...(body.url && { url: body.url }),
-        ...(body.title && { title: body.title }),
-        ...(body.platform && { platform: body.platform }),
-        ...(body.commLayer && { commLayer: body.commLayer }),
-        ...(body.sdkVersion && { sdkVersion: body.sdkVersion }),
-      },
-      function (err, batch) {
-        if (isDevelopment) {
-          console.log(batch);
-        }
+    if (!userIdHash) {
+      userIdHash = crypto.createHash('sha1').update(id).digest('hex');
+      userIdHashCache.set(id, userIdHash);
+    }
 
-        if (err) {
-          console.log(err);
-        }
-      },
-    );
+    const event = {
+      userId: userIdHash,
+      event: body.event,
+    };
 
-    return res.json({ success: true });
+    const properties = [
+      'url',
+      'title',
+      'platform',
+      'commLayer',
+      'commLayerVersion',
+      'sdkVersion',
+      'walletVersion',
+    ];
+
+    for (const property of properties) {
+      if (body[property]) {
+        event[property] = body[property];
+      }
+    }
+
+    analytics.track(event, function (err, batch) {
+      if (isDevelopment) {
+        console.log(batch);
+      }
+
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    return reply.send({ success: true });
   } catch (error) {
-    return res.json({ error });
+    return reply.send({ error });
   }
 });
 
@@ -143,26 +125,24 @@ io.on('connection', (socket) => {
   }
 
   socket.on('create_channel', async (id) => {
+    if (!uuid.validate(id)) {
+      return socket.emit(`message-${id}`, { error: 'must specify a valid id' });
+    }
+
     await rateLimiter.consume(socket.handshake.address);
 
     if (isDevelopment) {
       console.log('create channel', id);
     }
 
-    if (!uuid.validate(id)) {
-      return socket.emit(`message-${id}`, { error: 'must specify a valid id' });
-    }
-
     const room = io.sockets.adapter.rooms.get(id);
-    if (!id) {
-      return socket.emit(`message-${id}`, { error: 'must specify an id' });
-    }
 
     if (room) {
       return socket.emit(`message-${id}`, { error: 'room already created' });
     }
+
     socket.join(id);
-    return socket.emit(`channel_created-${id}`, id);
+    socket.emit(`channel_created-${id}`, id);
   });
 
   socket.on('message', async ({ id, message, context, plaintext }) => {
@@ -173,28 +153,20 @@ io.on('connection', (socket) => {
     }
 
     if (isDevelopment) {
-      // Minify encrypted message for easier readibility
+      // Minify encrypted message for easier readability
       let displayMessage = message;
       if (plaintext) {
         displayMessage = 'AAAAAA_ENCRYPTED_AAAAAA';
       }
 
-      if (context === 'mm-mobile') {
-        console.log(`\x1b[33m message-${id} -> \x1b[0m`, {
-          id,
-          context,
-          displayMessage,
-          plaintext,
-        });
-      } else {
-        console.log(`message-${id} -> `, {
-          id,
-          context,
-          displayMessage,
-          plaintext,
-        });
-      }
+      const logMessage =
+        context === 'mm-mobile'
+          ? `\x1b[33m message-${id} -> \x1b[0m`
+          : `message-${id} -> `;
+
+      console.log(logMessage, { id, context, displayMessage, plaintext });
     }
+
     socket.to(id).emit(`message-${id}`, { id, message });
   });
 
@@ -208,10 +180,15 @@ io.on('connection', (socket) => {
     if (isDevelopment) {
       console.log(`ping-${id} -> `, { id, context, message });
     }
+
     socket.to(id).emit(`ping-${id}`, { id, message });
   });
 
   socket.on('join_channel', async (id, test) => {
+    if (!uuid.validate(id)) {
+      return socket.emit(`message-${id}`, { error: 'must specify a valid id' });
+    }
+
     try {
       await rateLimiter.consume(socket.handshake.address);
     } catch (e) {
@@ -222,23 +199,13 @@ io.on('connection', (socket) => {
       console.log(`join_channel ${id} ${test}`);
     }
 
-    if (!uuid.validate(id)) {
-      socket.emit(`message-${id}`, { error: 'must specify a valid id' });
-      return;
-    }
-
     const room = io.sockets.adapter.rooms.get(id);
-    if (isDevelopment) {
-      console.log(`join_channel ${id} room.size=${room && room.size}`);
-    }
 
     if (room && room.size > 2) {
       if (isDevelopment) {
         console.log(`join_channel ${id} room already full`);
       }
-      socket.emit(`message-${id}`, { error: 'room already full' });
-      io.sockets.in(id).socketsLeave(id);
-      return;
+      return socket.emit(`message-${id}`, { error: 'room already full' });
     }
 
     socket.join(id);
@@ -247,12 +214,11 @@ io.on('connection', (socket) => {
       socket.emit(`clients_waiting_to_join-${id}`, room ? room.size : 1);
     }
 
-    socket.on('disconnect', function (error) {
+    socket.on('disconnect', (error) => {
       if (isDevelopment) {
         console.log(`disconnect event channel=${id}: `, error);
       }
       io.sockets.in(id).emit(`clients_disconnected-${id}`, error);
-      // io.sockets.in(id).socketsLeave(id);
     });
 
     if (room && room.size === 2) {
@@ -270,8 +236,61 @@ io.on('connection', (socket) => {
   });
 });
 
-// eslint-disable-next-line node/no-process-env
 const port = process.env.port || 4000;
-server.listen(port, () => {
-  console.log(`listening on *:${port}`);
-});
+
+// flushes all Segment events when Node process is interrupted for any reason
+// https://segment.com/docs/connections/sources/catalog/libraries/server/node/#long-running-process
+const exitGracefully = async (code) => {
+  console.log('Flushing events');
+  await analytics.flush(function (err) {
+    console.log('Flushed, and now this program can exit!');
+
+    if (err) {
+      console.log(err);
+    }
+    // eslint-disable-next-line node/no-process-exit
+    process.exit(code);
+  });
+};
+
+// Define a variable to track if the server is shutting down
+let isShuttingDown = false;
+
+// Function to perform cleanup operations before shutting down
+const cleanupAndExit = async () => {
+  if (isShuttingDown) {
+    return;
+  }
+
+  isShuttingDown = true;
+
+  try {
+    // Close the server
+    await server.close();
+    console.log('Server closed.');
+
+    // Perform any other necessary cleanup operations
+    exitGracefully(0);
+  } catch (err) {
+    console.error('Error during server shutdown:', err);
+    // eslint-disable-next-line node/no-process-exit
+    process.exit(1);
+  }
+};
+
+// Register event listeners for process termination events
+process.on('SIGINT', cleanupAndExit);
+process.on('SIGTERM', cleanupAndExit);
+
+const start = async () => {
+  try {
+    await fastify.listen(port);
+    console.log(`server listening on ${fastify.server.address().port}`);
+  } catch (err) {
+    console.error('Error starting the server:', err);
+    // eslint-disable-next-line node/no-process-exit
+    process.exit(1);
+  }
+};
+
+start();
