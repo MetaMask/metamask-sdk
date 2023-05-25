da4ff66003e27aec55821c3a57dbf97b6f6df8fd
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-05-25 10:37:49 +0200

    updates calls to use deeplink instead of universal links

--- packages/development-tools/sdk-chrome-extension/sdk-init.js
+++ packages/development-tools/sdk-chrome-extension/sdk-init.js
@@ -1,5 +1,6 @@
 const sdk = new MetaMaskSDK.MetaMaskSDK({
   forceInjectProvider: typeof window.ethereum === 'undefined',
+  useDeeplink: true,
   dappMetadata: {
     url: window.location.host,
     name: `${document.title} (MMSDK Ext)`,
