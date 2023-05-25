39edd95d1b84f8833b75e3f9257fb9782888c891
abretonc7s <107169956+abretonc7s@users.noreply.github.com>
2023-05-25 19:57:27 +0800

    Revert "Release 2.0.0" (#130)
    Revert "Release 2.0.0 (#129)"
    
    This reverts commit 9ca9b729f71fa5b23a7ad2ede808e4241ca05684.

--- packages/sdk-communication-layer/src/config.ts
+++ packages/sdk-communication-layer/src/config.ts
@@ -2,6 +2,7 @@ export const DEFAULT_SERVER_URL =
   'https://metamask-sdk-socket.metafi.codefi.network/';
 export const DEFAULT_SOCKET_TRANSPORTS = ['polling', 'websocket'];
 // TODO version should be parsed from package.json or set dynamically during build
+export const VERSION = '0.1.0';
 export const MIN_IN_MS = 1000 * 60;
 export const HOUR_IN_MS = MIN_IN_MS * 60;
 export const DAY_IN_MS = HOUR_IN_MS * 24;
