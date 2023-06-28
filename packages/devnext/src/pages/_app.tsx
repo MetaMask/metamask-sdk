0b7464f3a22279dbbf612197abba4aea55692d63
abretonc7s <arthur.breton@consensys.net>
2023-06-28 15:41:30 +0800

    chore: temp for publish

--- packages/devnext/src/pages/_app.tsx
+++ packages/devnext/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       forceDeleteProvider: true,
       forceInjectProvider: true,
       logging: {
-        developerMode: true,
+        developerMode: false,
         sdk: true,
         remoteLayer: false,
         serviceLayer: false,
