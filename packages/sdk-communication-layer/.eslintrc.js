b731564d7911025f8ea2cb7060465bc20931545e
abretonc7s <arthur.breton@consensys.net>
2023-06-30 10:45:48 +0800

    feat: improve promise handling

--- packages/sdk-communication-layer/.eslintrc.js
+++ packages/sdk-communication-layer/.eslintrc.js
@@ -1,6 +1,17 @@
-module.exports = {
-  extends: ['@metamask/eslint-config-typescript', '../../.eslintrc.js'],
+const path = require('path');
 
+console.debug(`TWH`, path.resolve(__dirname, 'tsconfig.eslint.json'));
+module.exports = {
+  extends: ['@metamask/eslint-config-typescript',
+      '../../.eslintrc.js',
+      // 'plugin:@typescript-eslint/recommended',
+      // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [path.resolve(__dirname, 'tsconfig.eslint.json')],
+  },
   overrides: [
     {
       files: ['**/*.ts'],
@@ -9,6 +20,7 @@ module.exports = {
           'error',
           'interface',
         ],
+        '@typescript-eslint/no-floating-promises': 'error',
         'import/no-named-as-default': 0,
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': ['error'],
@@ -17,9 +29,11 @@ module.exports = {
   ],
 
   ignorePatterns: [
-    '!.prettierrc.js',
-    '**/!.eslintrc.js',
+    '.prettierrc.js',
+    '**/.eslintrc.js',
+    '**/jest.config.ts',
     '**/dist*/',
+    'e2e/',
     'rollup.config.js',
   ],
 };
--- packages/sdk/.eslintrc.js
+++ packages/sdk/.eslintrc.js
@@ -1,6 +1,23 @@
+const path = require('path');
+
 module.exports = {
   extends: ['@metamask/eslint-config-typescript', '../../.eslintrc.js'],
 
+  root: true,
+
+  ignorePatterns: [
+    '.prettierrc.js',
+    '**/.eslintrc.js',
+    'jest.config.ts',
+    '**/dist*/',
+    'e2e/',
+    'rollup.config.js',
+  ],
+
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [path.resolve(__dirname, 'tsconfig.json')],
+  },
   overrides: [
     {
       files: ['**/*.ts'],
@@ -9,6 +26,7 @@ module.exports = {
           'error',
           'interface',
         ],
+        '@typescript-eslint/no-floating-promises': 'error',
         'import/no-named-as-default': 0,
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': ['error'],
@@ -20,12 +38,5 @@ module.exports = {
         'import/unambiguous': 'off',
       },
     },
-  ],
-
-  ignorePatterns: [
-    '!.prettierrc.js',
-    '**/!.eslintrc.js',
-    '**/dist*/',
-    'rollup.config.js',
   ],
 };
