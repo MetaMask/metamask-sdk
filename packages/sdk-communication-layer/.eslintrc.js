84d167c1f7717327b421baf6329f51f7f7139d45
abretonc7s <arthur.breton@consensys.net>
2023-07-03 10:10:07 +0800

    feat(sdk-communication-layer): improve linter to avoid dangling promises

--- packages/sdk-communication-layer/.eslintrc.js
+++ packages/sdk-communication-layer/.eslintrc.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
   extends: ['@metamask/eslint-config-typescript', '../../.eslintrc.js'],
-
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [path.resolve(__dirname, 'tsconfig.eslint.json')],
+  },
   overrides: [
     {
       files: ['**/*.ts'],
@@ -9,6 +18,8 @@ module.exports = {
           'error',
           'interface',
         ],
+        '@typescript-eslint/no-floating-promises': 'error',
+        'no-async-promise-executor': 'error',
         'import/no-named-as-default': 0,
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': ['error'],
@@ -17,9 +28,11 @@ module.exports = {
   ],
 
   ignorePatterns: [
-    '!.prettierrc.js',
-    '**/!.eslintrc.js',
+    '.prettierrc.js',
+    '**/.eslintrc.js',
+    '**/jest.config.ts',
     '**/dist*/',
+    'e2e/',
     'rollup.config.js',
   ],
 };
