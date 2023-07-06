fc2e71b19f45a73ec6234b2f24e02bd72c412d7e
abretonc7s <arthur.breton@consensys.net>
2023-07-06 21:08:26 +0800

    chore: linting

--- packages/devnext/src/pages/index.tsx
+++ packages/devnext/src/pages/index.tsx
@@ -1,17 +1,30 @@
-import { CommunicationLayerPreference, ConnectionStatus, EventType, ServiceStatus } from '@metamask/sdk-communication-layer';
+import {
+  CommunicationLayerPreference,
+  ConnectionStatus,
+  EventType,
+  ServiceStatus,
+} from '@metamask/sdk-communication-layer';
 import Head from 'next/head';
 import styles from 'src/styles/Home.module.css';
 import { MetaMaskSDK } from '@metamask/sdk';
 import { useEffect, useState } from 'react';
-import { MetaMaskInpageProvider } from "@metamask/providers";
+import { MetaMaskInpageProvider } from '@metamask/providers';
 import { ModalLoader } from '@metamask/sdk-install-modal-web';
 import Link from 'next/link';
 import { useSDK } from '../../../sdk-react/src/MetaMaskHooks';
 
 let _initialized = false;
 export default function Home() {
-  const { sdk, connected, connecting, status: serviceStatus, account, chainId, error } = useSDK();
-  const [response, setResponse] = useState<unknown>("");
+  const {
+    sdk,
+    connected,
+    connecting,
+    status: serviceStatus,
+    account,
+    chainId,
+    error,
+  } = useSDK();
+  const [response, setResponse] = useState<unknown>('');
 
   const connect = () => {
     if (!window.ethereum) {
@@ -19,7 +32,7 @@ export default function Home() {
     }
     window.ethereum
       .request({
-        method: "eth_requestAccounts",
+        method: 'eth_requestAccounts',
         params: [],
       })
       .then((accounts) => {
@@ -27,24 +40,24 @@ export default function Home() {
           console.debug(`connect:: accounts result`, accounts);
         }
       })
-      .catch((e) => console.log("request accounts ERR", e));
+      .catch((e) => console.log('request accounts ERR', e));
   };
 
   const sendTransaction = async () => {
-    const to = "0x0000000000000000000000000000000000000000";
+    const to = '0x0000000000000000000000000000000000000000';
     const transactionParameters = {
       to, // Required except during contract publications.
       from: window.ethereum?.selectedAddress, // must match user's active address.
-      value: "0x5AF3107A4000", // Only required to send ether to the recipient from the initiating external account.
+      value: '0x5AF3107A4000', // Only required to send ether to the recipient from the initiating external account.
     };
 
     try {
       // txHash is a hex string
       // As with any RPC call, it may throw an error
-      const txHash = await window.ethereum?.request({
-        method: "eth_sendTransaction",
+      const txHash = (await window.ethereum?.request({
+        method: 'eth_sendTransaction',
         params: [transactionParameters],
-      }) as string;
+      })) as string;
 
       setResponse(txHash);
     } catch (e) {
@@ -56,13 +69,13 @@ export default function Home() {
     const msgParams = JSON.stringify({
       domain: {
         // Defining the chain aka Rinkeby testnet or Ethereum Main Net
-        chainId: parseInt(window.ethereum?.chainId ?? "", 16),
+        chainId: parseInt(window.ethereum?.chainId ?? '', 16),
         // Give a user friendly name to the specific contract you are signing for.
-        name: "Ether Mail",
+        name: 'Ether Mail',
         // If name isn't enough add verifying contract to make sure you are establishing contracts with the proper entity
-        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+        verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
         // Just let's you know the latest version. Definitely make sure the field name is correct.
-        version: "1",
+        version: '1',
       },
 
       // Defining the message signing data content.
@@ -73,51 +86,51 @@ export default function Home() {
          - This is DApp Specific
          - Be as explicit as possible when building out the message schema.
         */
-        contents: "Hello, Bob!",
+        contents: 'Hello, Bob!',
         attachedMoneyInEth: 4.2,
         from: {
-          name: "Cow",
+          name: 'Cow',
           wallets: [
-            "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-            "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF",
+            '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+            '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
           ],
         },
         to: [
           {
-            name: "Bob",
+            name: 'Bob',
             wallets: [
-              "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-              "0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57",
-              "0xB0B0b0b0b0b0B000000000000000000000000000",
+              '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+              '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+              '0xB0B0b0b0b0b0B000000000000000000000000000',
             ],
           },
         ],
       },
       // Refers to the keys of the *types* object below.
-      primaryType: "Mail",
+      primaryType: 'Mail',
       types: {
         // TODO: Clarify if EIP712Domain refers to the domain the contract is hosted on
         EIP712Domain: [
-          { name: "name", type: "string" },
-          { name: "version", type: "string" },
-          { name: "chainId", type: "uint256" },
-          { name: "verifyingContract", type: "address" },
+          { name: 'name', type: 'string' },
+          { name: 'version', type: 'string' },
+          { name: 'chainId', type: 'uint256' },
+          { name: 'verifyingContract', type: 'address' },
         ],
         // Not an EIP712Domain definition
         Group: [
-          { name: "name", type: "string" },
-          { name: "members", type: "Person[]" },
+          { name: 'name', type: 'string' },
+          { name: 'members', type: 'Person[]' },
         ],
         // Refer to PrimaryType
         Mail: [
-          { name: "from", type: "Person" },
-          { name: "to", type: "Person[]" },
-          { name: "contents", type: "string" },
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person[]' },
+          { name: 'contents', type: 'string' },
         ],
         // Not an EIP712Domain definition
         Person: [
-          { name: "name", type: "string" },
-          { name: "wallets", type: "address[]" },
+          { name: 'name', type: 'string' },
+          { name: 'wallets', type: 'address[]' },
         ],
       },
     });
@@ -127,13 +140,15 @@ export default function Home() {
     console.debug(`sign from: ${from}`);
     try {
       if (!from || from === null) {
-        alert(`Invalid account -- please connect using eth_requestAccounts first`);
+        alert(
+          `Invalid account -- please connect using eth_requestAccounts first`,
+        );
         return;
       }
 
       const params = [from, msgParams];
-      const method = "eth_signTypedData_v4";
-      console.debug(`ethRequest ${method}`, JSON.stringify(params, null, 4))
+      const method = 'eth_signTypedData_v4';
+      console.debug(`ethRequest ${method}`, JSON.stringify(params, null, 4));
       console.debug(`sign params`, params);
       const resp = await window.ethereum?.request({ method, params });
       setResponse(resp);
@@ -142,13 +157,11 @@ export default function Home() {
     }
   };
 
-  const personalSign = async () => {
-
-  }
+  const personalSign = async () => {};
 
   const terminate = () => {
     sdk?.terminate();
-  }
+  };
 
   return (
     <>
@@ -165,44 +178,51 @@ export default function Home() {
         <p>
           Connection Status: <strong>{serviceStatus?.connectionStatus}</strong>
         </p>
-        {serviceStatus?.connectionStatus === ConnectionStatus.WAITING &&
-          <div>
-            Waiting for Metamask to link the connection...
-          </div>
-        }
+        {serviceStatus?.connectionStatus === ConnectionStatus.WAITING && (
+          <div>Waiting for Metamask to link the connection...</div>
+        )}
         <p>ChannelId: {serviceStatus?.channelConfig?.channelId}</p>
         <p>{`Expiration: ${serviceStatus?.channelConfig?.validUntil ?? ''}`}</p>
 
-        <div>
-          {`Connected: ${connected}`}
-        </div>
-        {connected ? <div>
-
+        <div>{`Connected: ${connected}`}</div>
+        {connected ? (
           <div>
-            {`Connected chain: ${chainId}`}
-            <p></p>
-            {`Connected account: ${account}`}
-            <p></p>
-            {`Last request response: ${response}`}
+            <div>
+              {`Connected chain: ${chainId}`}
+              <p></p>
+              {`Connected account: ${account}`}
+              <p></p>
+              {`Last request response: ${response}`}
+            </div>
+
+            <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+              Request Accounts
+            </button>
+
+            <button
+              style={{ padding: 10, margin: 10 }}
+              onClick={eth_signTypedData_v4}
+            >
+              eth_signTypedData_v4
+            </button>
+
+            <button
+              style={{ padding: 10, margin: 10 }}
+              onClick={sendTransaction}
+            >
+              sendTransaction
+            </button>
           </div>
-
+        ) : (
           <button style={{ padding: 10, margin: 10 }} onClick={connect}>
-            Request Accounts
+            Connect
           </button>
+        )}
 
-          <button style={{ padding: 10, margin: 10 }} onClick={eth_signTypedData_v4}>
-            eth_signTypedData_v4
-          </button>
-
-          <button style={{ padding: 10, margin: 10 }} onClick={sendTransaction}>
-            sendTransaction
-          </button>
-
-        </div> : <button style={{ padding: 10, margin: 10 }} onClick={connect}>
-          Connect
-        </button>}
-
-        <button style={{ padding: 10, margin: 10, backgroundColor: 'red' }} onClick={terminate} >
+        <button
+          style={{ padding: 10, margin: 10, backgroundColor: 'red' }}
+          onClick={terminate}
+        >
           Terminate
         </button>
       </main>
