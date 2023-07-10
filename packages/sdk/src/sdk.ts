17a3efcbb00317b54a0cc8c0fe693a834356e745
abretonc7s <arthur.breton@consensys.net>
2023-07-10 17:00:33 +0800

    refactor: remove multiple if layer

--- packages/sdk/src/sdk.ts
+++ packages/sdk/src/sdk.ts
@@ -216,19 +216,15 @@ export class MetaMaskSDK extends EventEmitter2 {
       storage.storageManager = getStorageManager(storage);
     }
 
-    if (platform.isBrowser()) {
-      if (!dappMetadata.base64Icon) {
-        // Try to extract default icon
-        if (platform.isBrowser()) {
-          const favicon = extractFavicon();
-          if (favicon) {
-            try {
-              const faviconUri = await getBase64FromUrl(favicon);
-              dappMetadata.base64Icon = faviconUri;
-            } catch (err) {
-              // Ignore favicon error.
-            }
-          }
+    if (platform.isBrowser() && !dappMetadata.base64Icon) {
+      // Try to extract default icon
+      const favicon = extractFavicon();
+      if (favicon) {
+        try {
+          const faviconUri = await getBase64FromUrl(favicon);
+          dappMetadata.base64Icon = faviconUri;
+        } catch (err) {
+          // Ignore favicon error.
         }
       }
     }
