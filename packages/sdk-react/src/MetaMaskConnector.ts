f835dbf5dac2b1a254b977c884f68172edbbf26c
abretonc7s <arthur.breton@consensys.net>
2023-07-10 15:22:35 +0800

    feat: add debug mode to metamaskconnector

--- packages/sdk-react/src/MetaMaskConnector.ts
+++ packages/sdk-react/src/MetaMaskConnector.ts
@@ -21,7 +21,8 @@ export type MetaMaskConnectorOptions = Pick<
    */
   // eslint-disable-next-line camelcase
   UNSTABLE_shimOnConnectSelectAccount?: boolean;
-  sdk?: MetaMaskSDK;
+  debug?: boolean;
+  sdk: MetaMaskSDK;
 };
 
 class MetaMaskConnector extends InjectedConnector {
@@ -30,6 +31,8 @@ class MetaMaskConnector extends InjectedConnector {
   protected shimDisconnectKey = `${this.id}.shimDisconnect`;
 
   #sdk: MetaMaskSDK;
+
+  #debug = false;
 
   #provider?: SDKProvider;
 
@@ -43,6 +46,7 @@ class MetaMaskConnector extends InjectedConnector {
     if (!options_?.sdk) {
       throw new Error('MetaMaskConnector requires MetaMaskSDK');
     }
+
     const { sdk } = options_;
     const sdkProvider = sdk.getProvider();
 
@@ -55,9 +59,11 @@ class MetaMaskConnector extends InjectedConnector {
       },
     };
 
-    console.debug(`initialize MetaMaskConnector`, options_);
     super({ chains, options });
 
+    if(options_?.debug) {
+      this.#debug = options_.debug;
+    }
     this.#sdk = sdk;
     this.#provider = sdkProvider;
     // Listen to sdk provider events and re-initialize events listeners accordingly
@@ -67,14 +73,7 @@ class MetaMaskConnector extends InjectedConnector {
     );
   }
 
-  private onSDKProviderUpdate(accounts: Address[]) {
-    console.debug(
-      `onSDKProviderUpdate listeners=${this.#provider?.listenerCount(
-        EventType.PROVIDER_UPDATE,
-      )}`,
-      accounts,
-    );
-
+  private onSDKProviderUpdate(_accounts: Address[]) {
     if (this.#provider) {
       // Cleanup previous handlers first
       this.#provider?.removeListener(
@@ -126,26 +125,23 @@ class MetaMaskConnector extends InjectedConnector {
       this.#provider = this.#sdk.getProvider();
 
       // check if previouslt disconnected -- if shims is removed it means disconnected
-      const shimDisconnect = this.storage?.getItem(this.shimDisconnectKey);
-      if (shimDisconnect) {
-        console.debug(`shims is not in storage --- should be connected`);
-      } else {
-        console.debug(`should be disconnected`);
-      }
+      // TODO see if we need to keep/use shimDisconnect
+      // const shimDisconnect = this.storage?.getItem(this.shimDisconnectKey);
 
       const accounts = (await this.#provider?.request({
         method: 'eth_requestAccounts',
         params: [],
       })) as Address[];
-      console.debug(`connect accounts`, accounts);
 
       const selectedAccount: Address = accounts?.[0] ?? '0x';
 
-      console.warn(
-        `TEST authorization ${Date.now()} authorized=${this.#sdk
-          ._getConnection()
-          ?.isAuthorized()}}`,
-      );
+      if(this.#debug) {
+        console.debug(
+          `MetaMaskConnector::connect() TEST authorization ${Date.now()} authorized=${this.#sdk
+            ._getConnection()
+            ?.isAuthorized()}}`,
+        );
+      }
 
       // backward compatibility with older wallet version that return accounts before authorization
       if (!this.#sdk._getConnection()?.isAuthorized()) {
@@ -155,7 +151,6 @@ class MetaMaskConnector extends InjectedConnector {
               ._getConnection()
               ?.getConnector()
               .once(EventType.AUTHORIZED, () => {
-                console.debug(`received auhtorized event`);
                 resolve(true);
               });
           });
@@ -163,42 +158,32 @@ class MetaMaskConnector extends InjectedConnector {
         await waitForAuthorized();
       }
 
-      console.debug(`provider`, this.#provider);
       let providerChainId: string | null | undefined = this.#provider?.chainId;
       if (!providerChainId) {
         // request chainId from provider
-        console.debug(`request chainId from provider`);
         try {
           providerChainId = (await this.#provider?.request({
             method: 'eth_chainId',
             params: [],
           })) as string;
-          console.debug(`provider chainId=${providerChainId}`);
         } catch (error) {
-          console.debug(`provider chainId error`, error);
+          console.warn(`MetamaskConnector::connect() provider chainId error -- reset to mainnet`, error);
           // use default mainnet provider
           providerChainId = '0x1';
         }
       }
 
-      console.debug(`provider chainId=${providerChainId}`);
       const chain = {
         id: parseInt(providerChainId, 16),
         unsupported: false,
       };
 
-      console.debug(
-        `checking chain chainId=${chainId} vs chain.id=${chain.id}`,
-      );
       if (chainId !== undefined && chain.id !== chainId) {
         const newChain = await this.switchChain(chainId);
         const unsupported = this.isChainUnsupported(newChain.id);
         chain.id = newChain.id;
         chain.unsupported = unsupported;
       }
-
-      // this.emit('connect', {})
-      // this.#provider?.emit('accountsChanged', [selectedAccount]);
 
       if (this.options?.shimDisconnect) {
         this.storage?.setItem(this.shimDisconnectKey, true);
