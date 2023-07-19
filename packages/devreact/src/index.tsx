e57a196609814a65a91b7c9ab9bbf4058a351667
abretonc7s <arthur.breton@consensys.net>
2023-07-19 19:52:48 +0800

    feat: move example to use hooks

--- packages/devreact/src/index.tsx
+++ packages/devreact/src/index.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { MetaMaskProvider } from '@metamask/sdk-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+     <MetaMaskProvider debug sdkOptions={{
+        logging:{
+          developerMode: true,
+        },
+        dappMetadata: {
+          name: "Demo React App",
+          url: window.location.host,
+        }
+    }}>
     <App />
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
--- packages/examples/create-react-app/sdk-copy.sh
+++ packages/examples/create-react-app/sdk-copy.sh
@@ -6,6 +6,7 @@ cd "$reldir/../../..";
 SDK_WORKSPACE_DIR="$( pwd; )";
 COMM_LAYER_DIR="$SDK_WORKSPACE_DIR/packages/sdk-communication-layer"
 SDK_DIR="$SDK_WORKSPACE_DIR/packages/sdk"
+SDK_REACT_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react"
 MODAL_DIR="$SDK_WORKSPACE_DIR/packages/sdk-install-modal-web"
 DAPP_DIR="$SDK_WORKSPACE_DIR/packages/examples/create-react-app"
 
@@ -15,12 +16,12 @@ echo "MODAL_DIR: $MODAL_DIR"
 echo "DAPPDIR_EXAMPLE_DIR: $DAPP_DIR"
 
 cd $DAPP_DIR
-echo "Hack Metamask sdk && sdk-communication-layer && sdk-install-modal-web packages..."
+echo "Hack Metamask sdk-react && sdk && sdk-communication-layer && sdk-install-modal-web packages..."
 ## hack to debug to latest unpublished version of the sdk
-rm -rf node_modules/@metamask/sdk-communication-layer node_modules/@metamask/sdk node_modules/@metamask/sdk-install-modal-web
+rm -rf node_modules/@metamask/sdk-communication-layer node_modules/@metamask/sdk node_modules/@metamask/sdk-install-modal-web node_modules/@metamask/sdk-react
 cp -rf $COMM_LAYER_DIR node_modules/@metamask/
 cp -rf $SDK_DIR node_modules/@metamask/
 cp -rf $MODAL_DIR node_modules/@metamask/
-
+cp -rf $SDK_REACT_DIR node_modules/@metamask/
 
 echo "All done."
