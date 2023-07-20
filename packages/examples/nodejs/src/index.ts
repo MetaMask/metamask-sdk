91f0ae6b033c4a8e55063184578fb37da10253e1
abretonc7s <arthur.breton@consensys.net>
2023-07-20 10:53:06 +0800

    feat: migrate nodejs demo to workspace

--- packages/examples/nodejs/src/index.ts
+++ packages/examples/nodejs/src/index.ts
@@ -8,6 +8,11 @@ const options: MetaMaskSDKOptions = {
   dappMetadata: {
     name: 'NodeJS example',
   },
+  logging: {
+    sdk: false
+  },
+  communicationServerUrl: 'http://192.168.50.114:4000',
+  checkInstallationImmediately: false,
   // Optional: customize modal text
   modals: {
     install: ({ link }) => {
@@ -31,86 +36,65 @@ const options: MetaMaskSDKOptions = {
 
 const sdk = new MetaMaskSDK(options);
 
-const start = async (ethereum: SDKProvider) => {
-  console.debug(`start dapp example`);
-
-  const accounts = await ethereum.request({
-    method: 'eth_requestAccounts',
-    params: [],
-  });
-
-  console.log('request accounts', accounts);
-
-  const msgParams = {
-    types: {
-      EIP712Domain: [
-        { name: 'name', type: 'string' },
-        { name: 'version', type: 'string' },
-        { name: 'chainId', type: 'uint256' },
-        { name: 'verifyingContract', type: 'address' },
-      ],
-      Person: [
-        { name: 'name', type: 'string' },
-        { name: 'wallet', type: 'address' },
-      ],
-      Mail: [
-        { name: 'from', type: 'Person' },
-        { name: 'to', type: 'Person' },
-        { name: 'contents', type: 'string' },
-      ],
+const msgParams = {
+  types: {
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallet', type: 'address' },
+    ],
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person' },
+      { name: 'contents', type: 'string' },
+    ],
+  },
+  primaryType: 'Mail',
+  domain: {
+    name: 'Ether Mail',
+    version: '1',
+    chainId: '0x1',
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+  },
+  message: {
+    from: {
+      name: 'Cow',
+      wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
     },
-    primaryType: 'Mail',
-    domain: {
-      name: 'Ether Mail',
-      version: '1',
-      chainId: '0x1',
-      verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    to: {
+      name: 'Bob',
+      wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
     },
-    message: {
-      from: {
-        name: 'Cow',
-        wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-      },
-      to: {
-        name: 'Bob',
-        wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-      },
-      contents: 'Hello, Bob!',
-    },
-  };
-
-  const from = accounts?.[0];
-
-  const signResponse = await ethereum.request({
-    method: 'eth_signTypedData_v3',
-    params: [from, JSON.stringify(msgParams)],
-  });
-
-  console.log('sign response', signResponse);
+    contents: 'Hello, Bob!',
+  },
 };
 
-sdk
-  .init()
-  .then(() => {
-    const ethereum = sdk.getProvider();
+const start = async () => {
+  console.debug(`start NodeJS example`);
 
-    const persistenceFileName = '.sdk-comm';
+  const accounts =  await sdk.connect();
 
-    if (options.storage?.enabled && fs.existsSync(persistenceFileName)) {
-      console.debug(
-        `Please open linked MetaMask mobile wallet for OTP or delete file '${persistenceFileName}' and scan QRCode.`,
-      );
-    }
+  console.log('connect request accounts', accounts);
 
-    ethereum.on('_initialized', () => {
-      start(ethereum);
+  const ethereum = sdk.getProvider();
+
+  ethereum.on('_initialized', async () => {
+    const from = accounts?.[0];
+
+    const signResponse = await ethereum.request({
+      method: 'eth_signTypedData_v3',
+      params: [from, JSON.stringify(msgParams)],
     });
 
-    ethereum.request({
-      method: 'eth_requestAccounts',
-      params: [],
-    });
-  })
-  .catch((error) => {
-    console.error(`sdk init error`, error);
+    console.log('sign response', signResponse);
   });
+};
+
+start().catch((err) => {
+  console.error(err);
+});
