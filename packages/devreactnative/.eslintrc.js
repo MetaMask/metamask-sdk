0a837a3fc8c78605a01d9f3cc0e8bddb34b2a708
abretonc7s <arthur.breton@consensys.net>
2023-09-01 14:29:35 +0800

    feat: improve devreact native setup for dynamic env

--- packages/devreactnative/.eslintrc.js
+++ packages/devreactnative/.eslintrc.js
@@ -1,4 +1,13 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parserOptions: {
+    project: [path.resolve(__dirname, 'tsconfig.json')],
+    babelOptions: {
+      configFile: path.resolve(__dirname, './babel.config.js'),
+    },
+  }
 };
