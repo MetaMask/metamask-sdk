9367aaaf2c644c99cc6baf82c2067a33aad72c16
Omri <omridan159@gmail.com>
2023-09-05 16:41:04 +0300

    feat: add unit tests to the RemoteCommunicationPostMessageStream class

---
+++ packages/sdk/src/services/RemoteCommunicationPostMessageStream/onMessage.test.ts
@@ -0,0 +1,74 @@
+import { RemoteCommunicationPostMessageStream } from '../../PostMessageStream/RemoteCommunicationPostMessageStream';
+import { ProviderConstants } from '../../constants';
+import { onMessage } from './onMessage';
+
+describe('onMessage', () => {
+  let instance: jest.Mocked<RemoteCommunicationPostMessageStream>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    instance = {
+      state: {
+        debug: false,
+      },
+      push: jest.fn(),
+    } as unknown as jest.Mocked<RemoteCommunicationPostMessageStream>;
+  });
+
+  it('should debug log if debug is enabled', () => {
+    const message = {
+      name: ProviderConstants.PROVIDER,
+      data: {},
+    };
+
+    instance.state.debug = true;
+    const consoleDebugSpy = jest.spyOn(console, 'debug').mockImplementation();
+
+    onMessage(instance, message);
+
+    expect(consoleDebugSpy).toHaveBeenCalledWith(
+      '[RCPMS] _onMessage ',
+      message,
+    );
+  });
+
+  it('should return early if message is not an object', () => {
+    const message = null as any;
+    onMessage(instance, message);
+    expect(instance.push).not.toHaveBeenCalled();
+  });
+
+  it('should return early if message data is not an object', () => {
+    const message = {
+      name: ProviderConstants.PROVIDER,
+      data: undefined,
+    };
+
+    onMessage(instance, message);
+
+    expect(instance.push).not.toHaveBeenCalled();
+  });
+
+  it('should return early if message name is not PROVIDER', () => {
+    const message = {
+      name: 'OTHER_PROVIDER',
+      data: {},
+    };
+
+    onMessage(instance, message);
+
+    expect(instance.push).not.toHaveBeenCalled();
+  });
+
+  it('should push message if all conditions are met', () => {
+    const message = {
+      name: ProviderConstants.PROVIDER,
+      data: {},
+    };
+
+    onMessage(instance, message);
+
+    expect(instance.push).toHaveBeenCalledWith(message);
+  });
+});
