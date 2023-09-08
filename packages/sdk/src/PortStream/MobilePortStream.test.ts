71c63824b54c5212d262c5881ff717581aa3324d
Omri Dan <61094771+omridan159@users.noreply.github.com>
2023-09-08 11:25:25 +0300

    feat: add unit tests to the MobilePortStream class (#335)
    * feat: add unit tests to the MetaMaskSDK class
    
    * feat: add unit tests to uncovered parts of the sdk package
    
    * feat: add more unit tests
    
    * feat: add more unit tests
    
    * feat: refactor the SDKProvider classand add unit tests to it
    
    * feat: organized the code and add description to the functions
    
    * chore: fix failing tests after merge with main
    
    * feat: refactor the MetaMaskInstaller class
    
    * feat: add unit tests to the MetaMaskInstaller class
    
    * feat: refactor the PlatformManager class
    
    * feat: add unit tests to the PlatformManager class
    
    * feat: add unit tests to the PlatformManager class extracted logic
    
    * feat: refactor the MobilePortStream class
    
    * feat: add unit tests to the MobilePortStream class
    
    * chore: clean the code
    
    * fix: remove default value to the 'platformType' property in the PlatformManager class

---
+++ packages/sdk/src/PortStream/MobilePortStream.test.ts
@@ -0,0 +1,96 @@
+import { Duplex } from 'stream';
+import * as onMessageModule from '../services/MobilePortStream/onMessage';
+import * as writeModule from '../services/MobilePortStream/write';
+import { MobilePortStream } from './MobilePortStream';
+
+describe('MobilePortStream', () => {
+  let mockOnMessage: jest.Mock;
+  let mockWrite: jest.Mock;
+  let mockAddEventListener: jest.Mock;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    mockOnMessage = jest.fn();
+    mockWrite = jest.fn();
+    mockAddEventListener = jest.fn();
+
+    jest.spyOn(onMessageModule, 'onMessage').mockImplementation(mockOnMessage);
+    jest.spyOn(writeModule, 'write').mockImplementation(mockWrite);
+
+    global.window = {
+      ...global.window,
+      addEventListener: mockAddEventListener,
+    } as any;
+
+    global.location = {
+      ...global.location,
+      origin: 'http://example.com',
+    } as any;
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should instantiate correctly', () => {
+    const port = { name: 'testPort' };
+    const stream = new MobilePortStream(port);
+
+    expect(stream).toBeInstanceOf(Duplex);
+    expect(stream._name).toBe('testPort');
+    expect(mockAddEventListener).toHaveBeenCalledWith(
+      'message',
+      expect.any(Function),
+      false,
+    );
+  });
+
+  it('should handle messages with _onMessage', () => {
+    const port = { name: 'testPort' };
+    const stream = new MobilePortStream(port);
+    const event = {
+      /* ... */
+    };
+
+    stream._onMessage(event);
+
+    expect(mockOnMessage).toHaveBeenCalledWith(stream, event);
+  });
+
+  it('should handle disconnect with _onDisconnect', () => {
+    const port = { name: 'testPort' };
+    const stream = new MobilePortStream(port);
+    const mockDestroy = jest.fn();
+    stream.destroy = mockDestroy;
+
+    stream._onDisconnect();
+
+    expect(mockDestroy).toHaveBeenCalled();
+  });
+
+  it('should handle _read', () => {
+    const port = { name: 'testPort' };
+    const stream = new MobilePortStream(port);
+
+    stream._read();
+
+    expect(true).toBe(true);
+  });
+
+  it('should handle _write', () => {
+    const port = { name: 'testPort' };
+    const stream = new MobilePortStream(port);
+    const chunk = {};
+    const encoding = 'utf-8';
+
+    stream._write(chunk, encoding, (err) => {
+      expect(err).toBeUndefined();
+      expect(mockWrite).toHaveBeenCalledWith(
+        chunk,
+        encoding,
+        expect.any(Function),
+      );
+    });
+  });
+});
