db84f8cbdddd55678c35ffc3ff01f163c574361e
Omri <omridan159@gmail.com>
2023-10-23 18:14:53 +0300

    feat: fix PR comments

--- packages/sdk/src/utils/get-browser-extension.ts
+++ packages/sdk/src/utils/get-browser-extension.ts
@@ -16,10 +16,11 @@ export async function getBrowserExtension({
     return baseProvider;
   } catch (e) {
     const { ethereum } = window as unknown as {
-      ethereum: {
-        isMetaMask?: boolean;
-        providers?: SDKProvider[];
-      };
+      ethereum:
+        | (SDKProvider & {
+            isMetaMask: boolean;
+          })
+        | { providers: any[] };
     };
 
     if (!ethereum) {
@@ -29,19 +30,19 @@ export async function getBrowserExtension({
     // The `providers` field is populated when CoinBase Wallet extension is also installed
     // The expected object is an array of providers, the MetaMask provider is inside
     // See https://docs.cloud.coinbase.com/wallet-sdk/docs/injected-provider-guidance for
-    if (Array.isArray(ethereum.providers)) {
-      const provider = mustBeMetaMask
-        ? ethereum.providers.find((p: any) => p.isMetaMask)
-        : ethereum.providers[0];
+    if ('providers' in ethereum) {
+      if (Array.isArray(ethereum.providers)) {
+        const provider = mustBeMetaMask
+          ? ethereum.providers.find((p: any) => p.isMetaMask)
+          : ethereum.providers[0];
 
-      if (!provider) {
-        throw new Error('No suitable provider found');
+        if (!provider) {
+          throw new Error('No suitable provider found');
+        }
+
+        return provider as SDKProvider;
       }
-
-      return provider;
-    }
-
-    if (mustBeMetaMask && !ethereum.isMetaMask) {
+    } else if (mustBeMetaMask && !ethereum.isMetaMask) {
       throw new Error('MetaMask provider not found in Ethereum');
     }
 
