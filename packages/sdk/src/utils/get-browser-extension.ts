b6c325d1e368bf96ef81f8f605ac89ce68b117af
Omri Dan <61094771+omridan159@users.noreply.github.com>
2023-10-24 13:17:15 +0300

    feat: add support to EIP-6963 protocol (#423)
    * feat: add support to eip6963 protocol
    
    * feat: fix PR comments
    
    * feat: fix PR comments
    
    * feat: fix unit tests
    
    * fix: the timeout logic
    
    * fix: pr comments
    
    * fix: pr comments
    
    * fix: tests

--- packages/sdk/src/utils/get-browser-extension.ts
+++ packages/sdk/src/utils/get-browser-extension.ts
@@ -1,36 +1,51 @@
-export function getBrowserExtension({
+import { SDKProvider } from '../provider/SDKProvider';
+import { eip6963RequestProvider } from './eip6963RequestProvider';
+
+export async function getBrowserExtension({
   mustBeMetaMask,
 }: {
   mustBeMetaMask: boolean;
-}) {
+}): Promise<SDKProvider> {
   if (typeof window === 'undefined') {
     throw new Error(`window not available`);
   }
 
-  const { ethereum } = window as { ethereum: any };
+  try {
+    const baseProvider = await eip6963RequestProvider();
 
-  if (!ethereum) {
-    throw new Error('Ethereum not found in window object');
-  }
+    return baseProvider;
+  } catch (e) {
+    const { ethereum } = window as unknown as {
+      ethereum:
+        | (SDKProvider & {
+            isMetaMask: boolean;
+          })
+        | { providers: any[] };
+    };
 
-  // The `providers` field is populated when CoinBase Wallet extension is also installed
-  // The expected object is an array of providers, the MetaMask provider is inside
-  // See https://docs.cloud.coinbase.com/wallet-sdk/docs/injected-provider-guidance for
-  if (Array.isArray(ethereum.providers)) {
-    const provider = mustBeMetaMask
-      ? ethereum.providers.find((p: any) => p.isMetaMask)
-      : ethereum.providers[0];
-
-    if (!provider) {
-      throw new Error('No suitable provider found');
+    if (!ethereum) {
+      throw new Error('Ethereum not found in window object');
     }
 
-    return provider;
-  }
+    // The `providers` field is populated when CoinBase Wallet extension is also installed
+    // The expected object is an array of providers, the MetaMask provider is inside
+    // See https://docs.cloud.coinbase.com/wallet-sdk/docs/injected-provider-guidance for
+    if ('providers' in ethereum) {
+      if (Array.isArray(ethereum.providers)) {
+        const provider = mustBeMetaMask
+          ? ethereum.providers.find((p: any) => p.isMetaMask)
+          : ethereum.providers[0];
 
-  if (mustBeMetaMask && !ethereum.isMetaMask) {
-    throw new Error('MetaMask provider not found in Ethereum');
-  }
+        if (!provider) {
+          throw new Error('No suitable provider found');
+        }
 
-  return ethereum;
+        return provider as SDKProvider;
+      }
+    } else if (mustBeMetaMask && !ethereum.isMetaMask) {
+      throw new Error('MetaMask provider not found in Ethereum');
+    }
+
+    return ethereum as SDKProvider;
+  }
 }
