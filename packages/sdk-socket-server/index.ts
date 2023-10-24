753f94ccf4ec1e76ba61d913afa7f4b1c25b0a19
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-10-24 16:39:54 +0200

    fix: fixes docker image running

--- packages/sdk-socket-server/index.ts
+++ packages/sdk-socket-server/index.ts
@@ -1,10 +1,11 @@
-import http from 'http';
 import dotenv from 'dotenv';
+dotenv.config();
+
+import http from 'http';
 import { app, analytics } from './api-config';
 import configureSocketIO from './socket-config';
 import { cleanupAndExit } from './utils';
 
-dotenv.config();
 const isDevelopment: boolean = process.env.NODE_ENV === 'development';
 
 const server = http.createServer(app);
@@ -13,10 +14,14 @@ configureSocketIO(server); // configure socket.io server
 console.log('INFO> isDevelopment?', isDevelopment);
 
 // Register event listeners for process termination events
-process.on('SIGINT', () => cleanupAndExit(server, analytics));
-process.on('SIGTERM', () => cleanupAndExit(server, analytics));
+process.on('SIGINT', async () => {
+  await cleanupAndExit(server, analytics);
+});
+process.on('SIGTERM', async () => {
+  await cleanupAndExit(server, analytics);
+});
 
-const port: number = Number(process.env.port) || 4000;
+const port: number = Number(process.env.PORT) || 4000;
 server.listen(port, () => {
   console.log(`INFO> listening on *:${port}`);
 });
