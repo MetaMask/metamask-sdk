3d1e76215190bad3cea6cf7744b8a9498f2dc0f3
abretonc7s <arthur.breton@consensys.net>
2023-10-24 17:08:14 +0800

    feat: disable preflight to prevent conflicting css

--- packages/sdk-react-ui/tailwind.config.js
+++ packages/sdk-react-ui/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  corePlugins: {
+    preflight: false,
+  },
   theme: {
     extend: {
       colors: {
