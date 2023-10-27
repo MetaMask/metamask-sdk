17290c166c97caf784b9eb3483efca61181dfaae
abretonc7s <107169956+abretonc7s@users.noreply.github.com>
2023-10-27 15:50:56 +0800

    feat: rename metamask_chainRPCs to metamask_batch and add unit tests (#440)
    * feat: rename to metamask_batch and add unit tests
    
    * chore: trigger project change to align release version
    
    * chore: temp rename
    
    * chore: temp rename
    
    * chore: temp rename

--- packages/sdk/src/config.ts
+++ packages/sdk/src/config.ts
@@ -12,14 +12,14 @@ export const METHODS_TO_REDIRECT: { [method: string]: boolean } = {
   wallet_addEthereumChain: true,
   wallet_switchEthereumChain: true,
   metamask_connectSign: true,
-  metamask_chainRPCs: true,
+  metamask_batch: true,
 };
 export const STORAGE_PATH = '.sdk-comm';
 export const STORAGE_PROVIDER_TYPE = 'providerType';
 export const RPC_METHODS = {
   METAMASK_GETPROVIDERSTATE: 'metamask_getProviderState',
   METAMASK_CONNECTSIGN: 'metamask_connectSign',
-  METAMASK_CHAINRPCS: 'metamask_chainRPCs',
+  METAMASK_BATCH: 'metamask_batch',
   PERSONAL_SIGN: 'personal_sign',
   ETH_REQUESTACCOUNTS: 'eth_requestAccounts',
   ETH_ACCOUNTS: 'eth_accounts',
