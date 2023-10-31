222401b754f3cb9f1b22fab027ddab7c56a2730e
Andrea Salvatore <andrea.salvatore@consensys.net>
2023-10-31 11:15:41 +0100

    feat: spawns socket.io server on one dedicated CPU and the REST API on all the other ones available

--- packages/sdk-socket-server/index.ts
+++ packages/sdk-socket-server/index.ts
@@ -1,27 +1,68 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
+import cluster from 'cluster';
+import os from 'os';
 import http from 'http';
 import { app, analytics } from './api-config';
 import configureSocketIO from './socket-config';
 import { cleanupAndExit } from './utils';
 
+dotenv.config();
+
 const isDevelopment: boolean = process.env.NODE_ENV === 'development';
+const numCPUs = os.cpus().length;
 
-const server = http.createServer(app);
-configureSocketIO(server); // configure socket.io server
+console.log('START> isDevelopment?', isDevelopment);
 
-console.log('INFO> isDevelopment?', isDevelopment);
+// Map to keep track of worker roles
+const workerRoles: Record<string, string> = {};
 
-// Register event listeners for process termination events
-process.on('SIGINT', async () => {
-  await cleanupAndExit(server, analytics);
-});
-process.on('SIGTERM', async () => {
-  await cleanupAndExit(server, analytics);
-});
+if (cluster.isPrimary) {
+  console.log(`START> Master ${process.pid} is running`);
 
-const port: number = Number(process.env.PORT) || 4000;
-server.listen(port, () => {
-  console.log(`INFO> listening on *:${port}`);
-});
+  // Fork a dedicated worker for Socket.io and record its role
+  const socketWorker = cluster.fork({ INSTANCE: 'socket' });
+  workerRoles[socketWorker.id] = 'socket';
+
+  // Fork workers for REST API for all CPUs except one and record their roles
+  for (let i = 0; i < numCPUs - 1; i++) {
+    const restWorker = cluster.fork({ INSTANCE: 'rest' });
+    workerRoles[restWorker.id] = 'rest';
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    if (workerRoles[worker.id] === 'rest') {
+      const newWorker = cluster.fork({ INSTANCE: 'rest' });
+      workerRoles[newWorker.id] = 'rest';
+    }
+    delete workerRoles[worker.id];
+  });
+} else {
+  const port: number = Number(process.env.PORT) || 4000;
+  const server = http.createServer(app);
+
+  if (process.env.INSTANCE === 'socket') {
+    // Dedicated instance for Socket.io
+    configureSocketIO(server);
+    console.log(
+      `START> SOCKET.IO server listening on *:${port}, worker ${process.pid}`,
+    );
+  } else {
+    // REST API servers
+    console.log(
+      `START> REST API listening on *:${port}, worker ${process.pid}`,
+    );
+  }
+
+  server.listen(port, () => {
+    console.log('START> Server started');
+  });
+
+  // Register event listeners for process termination events
+  process.on('SIGINT', async () => {
+    await cleanupAndExit(server, analytics);
+  });
+  process.on('SIGTERM', async () => {
+    await cleanupAndExit(server, analytics);
+  });
+}
