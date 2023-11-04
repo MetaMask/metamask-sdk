715611b027df2af3a8ea293fb40886d98794112d
abretonc7s <arthur.breton@consensys.net>
2023-11-04 15:35:52 +0800

    chore: organize imports

--- packages/sdk-communication-layer/src/index.ts
+++ packages/sdk-communication-layer/src/index.ts
@@ -1,3 +1,4 @@
+import { SendAnalytics } from './Analytics';
 import { ECIES, ECIESProps } from './ECIES';
 import {
   RemoteCommunication,
@@ -8,24 +9,23 @@ import {
   RPCMethodResult,
   SocketService,
 } from './SocketService';
+import { DEFAULT_SERVER_URL } from './config';
+import { AutoConnectOptions } from './types/AutoConnectOptions';
+import { AutoConnectType } from './types/AutoConnectType';
 import { ChannelConfig } from './types/ChannelConfig';
 import { CommunicationLayerMessage } from './types/CommunicationLayerMessage';
 import { CommunicationLayerPreference } from './types/CommunicationLayerPreference';
 import { ConnectionStatus } from './types/ConnectionStatus';
 import { DappMetadata } from './types/DappMetadata';
 import { DisconnectOptions } from './types/DisconnectOptions';
+import { EventType } from './types/EventType';
 import { KeyInfo } from './types/KeyInfo';
+import { CommunicationLayerLoggingOptions } from './types/LoggingOptions';
 import { MessageType } from './types/MessageType';
 import { OriginatorInfo } from './types/OriginatorInfo';
 import { PlatformType } from './types/PlatformType';
-import { WalletInfo } from './types/WalletInfo';
-import { SendAnalytics } from './Analytics';
-import { DEFAULT_SERVER_URL } from './config';
-import { AutoConnectOptions } from './types/AutoConnectOptions';
-import { AutoConnectType } from './types/AutoConnectType';
-import { EventType } from './types/EventType';
-import { CommunicationLayerLoggingOptions } from './types/LoggingOptions';
 import { ServiceStatus } from './types/ServiceStatus';
+import { WalletInfo } from './types/WalletInfo';
 // eslint-disable-next-line @typescript-eslint/no-shadow
 import { StorageManager, StorageManagerProps } from './types/StorageManager';
 import { TrackingEvents } from './types/TrackingEvent';
