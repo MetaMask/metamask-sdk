231eab57cfe30ccf1adadf91d7902bb49655c9fd
abretonc7s <arthur.breton@consensys.net>
2023-11-05 08:23:55 +0800

    feat: make unit test compatible

--- packages/sdk-communication-layer/src/services/RemoteCommunication/ConnectionManager/initCommunicationLayer.test.ts
+++ packages/sdk-communication-layer/src/services/RemoteCommunication/ConnectionManager/initCommunicationLayer.test.ts
@@ -135,7 +135,7 @@ describe('initCommunicationLayer', () => {
       instance,
     });
 
-    expect(mockOn).toHaveBeenCalledTimes(9); // because we have 9 events to listen to
+    expect(mockOn).toHaveBeenCalledTimes(10); // because we have 10 events to listen to
   });
 
   it('should pass otherPublicKey to SocketService if provided', () => {
