5423521a8bc60bb45fd450c64b10d7956617a0cc
abretonc7s <arthur.breton@consensys.net>
2023-11-07 19:08:00 +0800

    feat: set coverage goal to 50%

--- packages/sdk-communication-layer/jest.config.ts
+++ packages/sdk-communication-layer/jest.config.ts
@@ -5,10 +5,17 @@ module.exports = {
   coveragePathIgnorePatterns: ['./src/types', '/index.ts$'],
   coverageThreshold: {
     global: {
+<<<<<<< Updated upstream
       branches: 70,
       functions: 80,
       lines: 80,
       statements: 80,
+=======
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+>>>>>>> Stashed changes
     },
   },
   clearMocks: true,
--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -5,10 +5,17 @@ module.exports = {
   coveragePathIgnorePatterns: ['./src/types', '/index.ts$'],
   coverageThreshold: {
     global: {
+<<<<<<< Updated upstream
       branches: 68,
       functions: 72,
       lines: 75,
       statements: 78,
+=======
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+>>>>>>> Stashed changes
     },
   },
   clearMocks: true,
