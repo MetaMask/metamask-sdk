a2cbd0429608a5c606a094e812381d8d49ceafef
abretonc7s <107169956+abretonc7s@users.noreply.github.com>
2023-11-07 19:14:43 +0800

    feat: set minimum required coverage to 50/100 (#473)
    * feat: detect qrcode origin on wallet
    
    * feat: set coverage goal to 50%
    
    * feat: set coverage goal to 50%

--- packages/sdk-communication-layer/jest.config.ts
+++ packages/sdk-communication-layer/jest.config.ts
@@ -5,10 +5,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['./src/types', '/index.ts$'],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   },
   clearMocks: true,
--- packages/sdk-react/jest.config.ts
+++ packages/sdk-react/jest.config.ts
@@ -6,10 +6,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['./src/types', './src/index.ts'],
   coverageThreshold: {
     global: {
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
     },
   },
   clearMocks: true,
