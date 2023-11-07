2e90f5b3bcbd07069b7c6309a51661aed18ea97e
abretonc7s <arthur.breton@consensys.net>
2023-11-07 19:08:55 +0800

    feat: set coverage goal to 50%

--- packages/sdk-react/jest.config.ts
+++ packages/sdk-react/jest.config.ts
@@ -6,17 +6,10 @@ module.exports = {
   coveragePathIgnorePatterns: ['./src/types', './src/index.ts'],
   coverageThreshold: {
     global: {
-<<<<<<< Updated upstream
-      branches: 85,
-      functions: 85,
-      lines: 85,
-      statements: 85,
-=======
       branches: 50,
       functions: 50,
       lines: 50,
       statements: 50,
->>>>>>> Stashed changes
     },
   },
   clearMocks: true,
