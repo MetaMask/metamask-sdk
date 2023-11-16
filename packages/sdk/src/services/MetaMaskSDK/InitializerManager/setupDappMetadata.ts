417d55e352707581a00945912d49489274f1ec62
Omridan159 <Omridan159@gmail.com>
2023-11-16 14:48:44 +0200

    feat: fix unit tests

--- packages/sdk/src/services/MetaMaskSDK/InitializerManager/setupDappMetadata.ts
+++ packages/sdk/src/services/MetaMaskSDK/InitializerManager/setupDappMetadata.ts
@@ -1,17 +1,17 @@
 import { MetaMaskSDK } from '../../../sdk';
 
 /**
- * Sets up Dapp metadata for the MetaMask SDK instance.
+ * Validates and attaches Dapp metadata to a MetaMask SDK instance.
  *
- * This function configures the Dapp metadata by extracting the favicon and converting it to base64 format.
- * If a base64 icon is not already present in the options, it will try to extract a default favicon from the Dapp.
- * The processed metadata is then attached to the MetaMask SDK instance.
+ * This function checks 'iconUrl' and 'url' in the Dapp metadata, ensuring they start with 'http://' or 'https://'.
+ * If these URLs are incorrectly formatted, an error is thrown. Valid metadata is then attached to the SDK instance.
+ * Note: This function does not handle favicon extraction or base64 conversion.
  *
  * @param instance The MetaMaskSDK instance for which Dapp metadata will be set up.
  * @returns void
- * @async
  */
-export async function setupDappMetadata(instance: MetaMaskSDK) {
+
+export function setupDappMetadata(instance: MetaMaskSDK) {
   const { options } = instance;
 
   // Check if iconUrl and url are valid
