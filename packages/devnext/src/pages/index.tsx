56d114fa464e0b322a4e82c2416b2dec1b74d30c
abretonc7s <arthur.breton@consensys.net>
2023-11-16 14:46:53 +0800

    feat: save working state

--- packages/devnext/src/pages/index.tsx
+++ packages/devnext/src/pages/index.tsx
@@ -1,20 +1,42 @@
 import Link from 'next/link';
-import { First, ItemView } from '@metamask/sdk-ui';
+import { FABAccount, First, IconSimplified, ItemView } from '@metamask/sdk-ui';
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href={'demo'}>Main demo</Link>
-      </div>
-      <div>
-        <Link href={'uikit'}>UI Kit demo</Link>
-      </div>
-      <div style={{ margin: 20 }}>
-        <First />
-      </div>
-      <div style={{ margin: 20 }}>
-        <ItemView processing={true} label="label" value="value" />
+      <div style={{ height: '100vh' }}>
+        <div
+          style={{
+            display: 'flex',
+            marginTop: 20,
+            marginBottom: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 10,
+          }}
+        >
+          <div style={{ padding: 20, border: '1px solid' }}>
+            <Link href={'demo'}>Main demo</Link>
+          </div>
+          <div style={{ padding: 20, border: '1px solid' }}>
+            <Link href={'uikit'}>UI Kit demo</Link>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <h2>Testing UI Components</h2>
+          <div style={{ margin: 20 }}>
+            <First />
+          </div>
+          <div style={{ margin: 20, backgroundColor: 'black' }}>
+            <IconSimplified color={'white'} />
+            <IconSimplified color={'orange'} />
+          </div>
+          <div style={{ margin: 20 }}>
+            <ItemView processing={true} label="label" value="value" />
+          </div>
+          <FABAccount />
+        </div>
       </div>
     </div>
   );
--- packages/sdk-ui/src/index.ts
+++ packages/sdk-ui/src/index.ts
@@ -1,8 +1,11 @@
 import { First } from './first/first';
 import { ItemView } from './item-view/item-view';
+import { FABAccount } from './fab-account/fab-account';
+import { IconSimplified } from './icons/IconsSimplified';
+import { MetaMaskButton } from './metamask-button/metamask-button';
 
 const testMe = () => {
   console.log('Hi, I have been tested.');
 };
 
-export { First, testMe, ItemView };
+export { First, testMe, ItemView, FABAccount, IconSimplified, MetaMaskButton };
