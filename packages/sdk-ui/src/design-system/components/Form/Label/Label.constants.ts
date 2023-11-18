4300f392ccf7392bdeb882693225fd16e4e55815
abretonc7s <arthur.breton@consensys.net>
2023-11-18 15:02:11 +0800

    feat: add component Form

---
+++ packages/sdk-ui/src/design-system/components/Form/Label/Label.constants.ts
@@ -0,0 +1,11 @@
+// Internal dependencies.
+import { TextVariant } from '../../Texts/Text';
+
+// Defaults
+export const DEFAULT_LABEL_TEXT_VARIANT = TextVariant.BodyMD;
+
+// Test IDs
+export const LABEL_TEST_ID = 'label';
+
+// Sample consts
+export const SAMPLE_LABEL_TEXT = 'Sample Label Text';
