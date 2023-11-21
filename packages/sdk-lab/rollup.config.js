8a43a0a163afd8b9eabadfe7cdf230df5d498327
abretonc7s <arthur.breton@consensys.net>
2023-11-22 00:06:28 +0800

    feat: build setup for ui and lab

---
+++ packages/sdk-lab/rollup.config.js
@@ -0,0 +1,46 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json';
+
+
+const packageJson = require('./package.json');
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config =
+  [
+    {
+      external: ["react", "react-dom"],
+      input: 'src/index.ts',
+      output: [
+        {
+          file: packageJson.module,
+          inlineDynamicImports: true,
+          format: 'esm',
+          sourcemap: false,
+          sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+            // Not sure why rollup otherwise adds an extra '../' to the path
+
+            // Adjust the path transformation logic as needed
+            return relativeSourcePath.replace(/^..\//, '');
+          },
+        },
+      ],
+      plugins: [
+        external(),
+        nodeResolve({
+          browser: true,
+        }),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        json(),
+        terser(),
+      ],
+    },
+  ];
+
+export default config;
---
+++ packages/sdk-ui/rollup.config.js
@@ -0,0 +1,48 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json';
+import image from '@rollup/plugin-image';
+
+
+const packageJson = require('./package.json');
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config =
+  [
+    {
+      external: ["react", "react-dom"],
+      input: 'src/index.ts',
+      output: [
+        {
+          file: packageJson.module,
+          inlineDynamicImports: true,
+          format: 'esm',
+          sourcemap: true,
+          sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+            // Not sure why rollup otherwise adds an extra '../' to the path
+
+            // Adjust the path transformation logic as needed
+            return relativeSourcePath.replace(/^..\//, '');
+          },
+        },
+      ],
+      plugins: [
+        external(),
+        image(),
+        nodeResolve({
+          browser: true,
+        }),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json', allowJs: true }),
+        json(),
+        terser(),
+      ],
+    },
+  ];
+
+export default config;
