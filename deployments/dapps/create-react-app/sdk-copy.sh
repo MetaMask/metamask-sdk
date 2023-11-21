66e544545b7eec717b05d76724123fba6097bc30
abretonc7s <arthur.breton@consensys.net>
2023-11-22 00:11:43 +0800

    feat: lockfile and copy script utility

--- deployments/dapps/create-react-app/sdk-copy.sh
+++ deployments/dapps/create-react-app/sdk-copy.sh
@@ -8,6 +8,8 @@ COMM_LAYER_DIR="$SDK_WORKSPACE_DIR/packages/sdk-communication-layer"
 SDK_DIR="$SDK_WORKSPACE_DIR/packages/sdk"
 SDK_REACT_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react"
 SDK_REACT_UI_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react-ui"
+SDK_UI_DIR="$SDK_WORKSPACE_DIR/packages/sdk-ui"
+SDK_LAB_DIR="$SDK_WORKSPACE_DIR/packages/sdk-lab"
 SDK_INSTALL_MODAL_WEB_DIR="$SDK_WORKSPACE_DIR/packages/sdk-install-modal-web"
 
 DAPP_DIR="$SDK_WORKSPACE_DIR/deployments/dapps/create-react-app"
@@ -16,7 +18,8 @@ echo "SDK_DIR: $SDK_DIR"
 echo "COMM_LAYER_DIR: $COMM_LAYER_DIR"
 echo "DAPP_DIR: $DAPP_DIR"
 echo "SDK_REACT_DIR: $SDK_REACT_DIR"
-echo "SDK_REACT_UI_DIR: $SDK_REACT_UI_DIR"
+echo "SDK_UI_DIR: $SDK_UI_DIR"
+echo "SDK_LAB_DIR: $SDK_LAB_DIR"
 echo "SDK_INSTALL_MODAL_WEB_DIR: $SDK_INSTALL_MODAL_WEB_DIR"
 
 echo "########### START REPLACING SDK_COMMUNICATION_LAYER #########"
@@ -24,11 +27,13 @@ echo "########### START REPLACING SDK_COMMUNICATION_LAYER #########"
 cd $DAPP_DIR
 echo "Hack Metamask sdk && sdk-communication-layer packages..."
 ## hack to debug to latest unpublished version of the sdk
-rm -rf node_modules/@metamask/sdk-communication-layer node_modules/@metamask/sdk node_modules/@metamask/sdk-react node_modules/@metamask/sdk-react-ui node_modules/@metamask/sdk-install-modal-web
+rm -rf node_modules/@metamask/sdk-communication-layer node_modules/@metamask/sdk node_modules/@metamask/sdk-react node_modules/@metamask/sdk-react-ui node_modules/@metamask/sdk-ui node_modules/@metamask/sdk-lab node_modules/@metamask/sdk-install-modal-web
 cp -rf $COMM_LAYER_DIR node_modules/@metamask/
 cp -rf $SDK_DIR node_modules/@metamask/
 cp -rf $SDK_REACT_DIR node_modules/@metamask/
 cp -rf $SDK_REACT_UI_DIR node_modules/@metamask/
+cp -rf $SDK_UI_DIR node_modules/@metamask/
+cp -rf $SDK_LAB_DIR node_modules/@metamask/
 cp -rf $SDK_INSTALL_MODAL_WEB_DIR node_modules/@metamask/
 
 
