c9eb802a35a3c30cd2cb33cb8c26f52fbbb29dc7
Omridan159 <Omridan159@gmail.com>
2023-11-21 13:27:20 +0200

    feat: add favicon fallbcack logic

--- packages/sdk/src/services/MetaMaskSDK/InitializerManager/setupDappMetadata.ts
+++ packages/sdk/src/services/MetaMaskSDK/InitializerManager/setupDappMetadata.ts
@@ -1,4 +1,5 @@
 import { MetaMaskSDK } from '../../../sdk';
+import { extractFavicon } from '../../../utils/extractFavicon';
 
 /**
  * Validates and attaches Dapp metadata to a MetaMask SDK instance.
@@ -11,21 +12,25 @@ import { MetaMaskSDK } from '../../../sdk';
  * @returns void
  */
 
+const BASE_64_ICON_MAX_LENGTH = 163400;
+
 export function setupDappMetadata(instance: MetaMaskSDK) {
   const { options } = instance;
 
   // Check if iconUrl and url are valid
   // eslint-disable-next-line require-unicode-regexp
-  const urlPattern = /^(http|https):\/\/[^\s]*|localhost(:\d+)?$/; // Regular expression for URLs starting with http:// or https://
+  const urlPattern = /^(http|https):\/\/[^\s]*$/; // Regular expression for URLs starting with http:// or https://
 
   if (options.dappMetadata) {
     if (
       options.dappMetadata.iconUrl &&
       !urlPattern.test(options.dappMetadata.iconUrl)
     ) {
-      throw new Error(
+      console.warn(
         'Invalid dappMetadata.iconUrl: URL must start with http:// or https://',
       );
+
+      options.dappMetadata.iconUrl = undefined;
     }
 
     // This check ensures that the base64Icon string in the dappMetadata does not exceed 163,400 characters.
@@ -33,20 +38,32 @@ export function setupDappMetadata(instance: MetaMaskSDK) {
     // Keeping the base64Icon string length below this threshold ensures reliable communication and functionality.
     if (
       options.dappMetadata.base64Icon &&
-      options.dappMetadata.base64Icon.length > 163400
+      options.dappMetadata.base64Icon.length > BASE_64_ICON_MAX_LENGTH
     ) {
-      throw new Error(
+      console.warn(
         'Invalid dappMetadata.base64Icon: Base64-encoded icon string length must be less than 163400 characters',
       );
+
+      options.dappMetadata.base64Icon = undefined;
     }
 
     if (
       options.dappMetadata.url &&
       !urlPattern.test(options.dappMetadata.url)
     ) {
-      throw new Error(
+      console.warn(
         'Invalid dappMetadata.url: URL must start with http:// or https://',
       );
+    }
+
+    const favicon = extractFavicon();
+
+    if (
+      favicon &&
+      !options.dappMetadata.iconUrl &&
+      !options.dappMetadata.base64Icon
+    ) {
+      options.dappMetadata.iconUrl = favicon;
     }
   }
 
