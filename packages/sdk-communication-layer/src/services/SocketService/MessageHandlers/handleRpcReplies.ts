922bce93b7b161762117c3c2d10892a5c7047c70
abretonc7s <arthur.breton@consensys.net>
2023-11-25 18:42:54 +0800

    feat: cleanup

--- packages/sdk-communication-layer/src/services/SocketService/MessageHandlers/handleRpcReplies.ts
+++ packages/sdk-communication-layer/src/services/SocketService/MessageHandlers/handleRpcReplies.ts
@@ -3,6 +3,12 @@ import { SocketService } from '../../../SocketService';
 import { CommunicationLayerMessage } from '../../../types/CommunicationLayerMessage';
 import { waitForNextRpcCall, waitForRpc } from '../../../utils/wait';
 
+const DEFAULT_SKIPPED_RPC_ERROR = 'SDK_CONNECTION_ISSUE';
+enum PromiseType {
+  RPC_CHECK = 'rpcCheck',
+  SKIPPED_RPC = 'skippedRpc',
+}
+
 /**
  * Handles the waiting for RPC replies for the provided message.
  * If the instance is the originator of the message and the message has an associated RPC ID,
@@ -25,22 +31,20 @@ export async function handleRpcReplies(
         rpcId,
         instance.state.rpcMethodTracker,
         200,
-      ).then((result) => ({ type: 'rpcCheck', result }));
+      ).then((result) => ({ type: PromiseType.RPC_CHECK, result }));
 
-      // Check for missed rpc call which can happen on slow mobile to mobile connection.
+      // Check for missed rpc calls which could historically happen on older wallet version.
       // In this case manually trigger an error.
       const checkForSkippedRpcPromise = (async () => {
         // wait for next rpc call to be sent and defined.
         const nextRpcId = await waitForNextRpcCall({ instance, rpcId });
-        console.log(`current=${rpcId} ---> nextRpcId=${nextRpcId}`);
-
         const result = await waitForRpc(
           nextRpcId,
           instance.state.rpcMethodTracker,
           200,
         );
-        // It means rpcId has been skipped!
-        return { type: 'skippedRpc', result };
+        // If it returns before a previous rpc calls, it means the previous one was skipped.
+        return { type: PromiseType.SKIPPED_RPC, result };
       })();
 
       const winner = await Promise.race([
@@ -48,9 +52,8 @@ export async function handleRpcReplies(
         checkForSkippedRpcPromise,
       ]);
 
-      if (winner.type === 'rpcCheck') {
+      if (winner.type === PromiseType.RPC_CHECK) {
         const rpcCheck = winner.result;
-        console.debug(`AAAAAAAAAAAAAAAAAAAAA okokok`, rpcCheck);
         // rpcCheck resolved first, handle normally
         if (instance.state.debug) {
           console.debug(
@@ -58,22 +61,21 @@ export async function handleRpcReplies(
             rpcCheck.result,
           );
         }
-      } else if (winner.type === 'skippedRpc') {
+      } else if (winner.type === PromiseType.SKIPPED_RPC) {
         const { result } = winner;
         // set the rpc has timedout and error.
-        console.error(
-          `AAAAAAAAAAAAAAAAAAAAA RPC METHOD HAS BEEN SKIPPED rpcid=${rpcId} method=${method}`,
+        console.warn(
+          `[handleRpcReplies] RPC METHOD HAS BEEN SKIPPED rpcid=${rpcId} method=${method}`,
           result,
         );
 
         const rpcResult = {
           ...instance.state.rpcMethodTracker[rpcId],
-          error: new Error('SKIPPED'),
+          error: new Error(DEFAULT_SKIPPED_RPC_ERROR),
         };
         instance.emit(EventType.RPC_UPDATE, rpcResult);
 
-        // FIXME should also emit message event?
-        // simulate wallet error message
+        // simulate wallet error message if a message was skipped.
         const errorMessage = {
           data: { ...rpcResult, jsonrpc: '2.0' },
           name: 'metamask-provider',
@@ -84,6 +86,7 @@ export async function handleRpcReplies(
       }
     } catch (err) {
       console.warn(`Error rpcId=${message.id} ${method}`, err);
+      throw err;
     }
   }
 }
