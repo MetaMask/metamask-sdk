24fc5148b2bf29a19b37e67f463162944994f991
Christopher Ferreira <christopher.ferreira@consensys.net>
2023-12-18 14:34:57 +0000

    chore: adds WDIO constants

--- e2e/src/Constants.ts
+++ e2e/src/Constants.ts
@@ -27,3 +27,10 @@ export const METAMASK_BUNDLE_ID = process.env.BUNDLE_ID as string;
 
 export const METAMASK_APP_NAME_ANDROID =
   METAMASK_BUNDLE_ID === 'io.metamask.qa' ? 'MetaMask-QA' : 'MetaMask';
+
+export const WDIO_IOS_CLASS_CHAIN = '-ios class chain:';
+export const WDIO_IOS_PREDICATE_STRING = '-ios predicate string:';
+export const WDIO_ANDROID_UI_AUTOMATOR = 'android=';
+export const WDIO_XPATH = '';
+export const WDIO_ACCESSIBILITY_ID = '~';
+export const WDIO_RESOURCE_ID = 'id:';
