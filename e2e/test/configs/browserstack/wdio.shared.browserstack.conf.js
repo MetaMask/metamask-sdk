3c71a6bba1086abb042e170cb24e42301a019fd9
Omridan159 <omridan159@gmail.com>
2024-01-09 16:01:26 +0200

    feat: fix CI job

---
+++ e2e/test/configs/browserstack/wdio.shared.browserstack.conf.js
@@ -0,0 +1,40 @@
+/* eslint-disable node/no-process-env */
+const { config } = require('../wdio.conf.ts');
+
+const username = process.env.BROWSERSTACK_USERNAME;
+const accessKey = process.env.BROWSERSTACK_ACCESS_KEY;
+
+config.services = (config.services ? config.services : []).concat([
+  [
+    'browserstack',
+    {
+      testObservability: true,
+      testObservabilityOptions: {
+        projectName: 'SDK e2e tests',
+        buildName: 'SDK e2e PR regression',
+      },
+      browserstackLocal: true,
+    },
+  ],
+]);
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+config.commonCapabilities = {
+  'bstack:options': {
+    buildName: 'SDK e2e PR regression',
+    buildIdentifier: '123',
+    projectName: 'BrowserStack Sample',
+    debug: 'true',
+    networkLogs: 'false',
+    consoleLogs: 'info',
+    deviceOrientation: 'portrait',
+    screenOrientation: 'locked',
+  },
+};
+
+// Browserstack credentials
+config.user = username;
+config.key = accessKey;
+
+module.exports = config;
