14be2dfda214308087efbab54280241a0999064b
Omridan159 <omridan159@gmail.com>
2024-01-09 17:14:58 +0200

    feat: fix CI job

---
+++ e2e/test/configs/browserstack/wdio.android.app.browserstack.conf.ts
@@ -0,0 +1,28 @@
+/* eslint-disable node/no-process-env */
+import path from 'path';
+import dotenv from 'dotenv';
+import config from './wdio.shared.browserstack.conf';
+
+dotenv.config({ path: path.join(process.cwd(), '.android.env') });
+
+config.capabilities = [
+  {
+    // The defaults you need to have in your config
+    platformName: 'Android',
+    maxInstances: 1,
+    interactiveDebugging: true,
+    'appium:deviceName': 'Google Pixel 7',
+    'appium:platformVersion': '13.0',
+    'appium:automationName': 'UiAutomator2',
+    'appium:app': process.env.APP_PATH,
+    'appium:appActivity': 'io.metamask.MainActivity',
+    'appium:newCommandTimeout': 360,
+    'appium:appPackage': process.env.BUNDLE_ID,
+    /* This setting will tell Appium if it need to install the app or no. */
+    'appium:noReset': false,
+    // 'appium:optionalIntentArguments': '--es fixtureServerPort 12345'
+  },
+];
+
+// eslint-disable-next-line node/exports-style
+exports.config = config;
