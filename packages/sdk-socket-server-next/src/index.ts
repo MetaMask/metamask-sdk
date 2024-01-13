11bf94d8f4b0af893adccd28e3c95fbae2c9950e
abretonc7s <arthur.breton@consensys.net>
2024-01-13 12:00:22 +0800

    feat: metrics

--- packages/sdk-socket-server-next/src/index.ts
+++ packages/sdk-socket-server-next/src/index.ts
@@ -9,6 +9,7 @@ import { analytics, app } from './api-config';
 import { logger } from './logger';
 import { cleanupAndExit } from './utils';
 import { configureSocketServer } from './socket-config';
+import { extractMetrics } from './metrics';
 
 export const isDevelopment: boolean = process.env.NODE_ENV === 'development';
 export const isDevelopmentServer: boolean =
@@ -32,17 +33,8 @@ configureSocketServer(server)
     // Make sure to protect the endpoint to be only available within the cluster for prometheus
     app.get('/metrics', async (_req, res) => {
       res.set('Content-Type', 'text/plain');
-      const totalClients = ioServer.engine.clientsCount;
-      let fullMetrics = `# HELP socket_io_server_total_clients Total number of connected clients\n`;
-      fullMetrics += `# TYPE socket_io_server_total_clients gauge\n`;
-      fullMetrics += `socket_io_server_total_clients ${totalClients}\n`;
-
-      const totalRooms = ioServer.sockets.adapter.rooms.size;
-      fullMetrics += `# HELP socket_io_server_total_rooms Total number of rooms\n`;
-      fullMetrics += `# TYPE socket_io_server_total_rooms gauge\n`;
-      fullMetrics += `socket_io_server_total_rooms ${totalRooms}\n`;
-
-      res.send(fullMetrics);
+      const metrics = extractMetrics({ ioServer });
+      res.send(metrics);
     });
 
     const port: number = Number(process.env.PORT) || 4000;
