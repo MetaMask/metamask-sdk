e8d3fee10880ed8ddbbdf62b3b06fbc4c2e20953
Omridan159 <omridan159@gmail.com>
2024-02-06 09:22:09 +0200

    chore: add connect and sign method to wagmi demo dapp

--- packages/examples/wagmi-demo/src/app/page.tsx
+++ packages/examples/wagmi-demo/src/app/page.tsx
@@ -1,150 +1,193 @@
 'use client';
 // Import necessary hooks and utilities from wagmi and ethers (for signing messages)
-import { useAccount, useConnect, useDisconnect, useSwitchChain, useSignMessage, useSendTransaction } from 'wagmi';
+import {
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useSwitchChain,
+  useSignMessage,
+  useSendTransaction,
+} from 'wagmi';
 import { ethers } from 'ethers';
 
 function App() {
-   const account = useAccount();
-   const { connectors, connect, error: connectError } = useConnect();
-   const { disconnect } = useDisconnect();
-   const { chains, switchChain, error: switchChainError } = useSwitchChain();
-   const { signMessage, error: signError } = useSignMessage();
-   const { sendTransaction, error: txError } = useSendTransaction();
+  const account = useAccount();
+  const {
+    connectors,
+    connect,
+    connectAsync,
+    error: connectError,
+  } = useConnect();
+  const { disconnect } = useDisconnect();
+  const { chains, switchChain, error: switchChainError } = useSwitchChain();
+  const { signMessage, error: signError, signMessageAsync } = useSignMessage();
+  const { sendTransaction, error: txError } = useSendTransaction();
 
-   // Example send transaction function
-   const sendTx = async () => {
-      if (!account?.isConnected) return;
+  // Example send transaction function
+  const sendTx = async () => {
+    if (!account?.isConnected) return;
 
-      await sendTransaction({
-         to: '0x0000000000000000000000000000000000000000',
-         value: ethers.parseEther('0.01') // Send 0.01 ETH
-      });
-   };
+    await sendTransaction({
+      to: '0x0000000000000000000000000000000000000000',
+      value: ethers.parseEther('0.01'), // Send 0.01 ETH
+    });
+  };
 
-   // Example sign message function
-   const signAMessage = async () => {
-      if (!account?.isConnected) return;
+  // Example sign message function
+  const signAMessage = async () => {
+    if (!account?.isConnected) return;
 
-      await signMessage({
-         message: 'Hello, world!'
-      });
-   };
+    await signMessage({
+      message: 'Hello, world!',
+    });
+  };
 
-   return (
-      <div style={styles.container}>
-         <div style={styles.info}>
-            <h2 style={styles.title}>Account</h2>
-            <div>
-               <br />
-               Address: {account?.address}
-               <br />
-               ChainId: {account.chainId}
-               <br />
-               Current Chain: {account.chain?.name}
-               <br />
-               <span
-                  style={{
-                     color: account?.isConnected ? 'green' : 'red',
-                     fontWeight: 'bold'
-                  }}
-               >
-                  Status: {account?.isConnected ? 'Connected' : 'Disconnected'}
-               </span>
-            </div>
-            {!account?.isConnected ? (
-               <>
-                  <button
-                     style={{ ...styles.button, backgroundColor: 'orange' }}
-                     key={connectors[0].id}
-                     onClick={() => connect({ connector: connectors[0] })}
-                  >
-                     Connect To {connectors[0].name}
-                  </button>
+  const connectAndSign = async () => {
+    if (!account?.isConnected) {
+      await connectAsync({ connector: connectors[0] });
+    }
 
-                  <div>{connectError?.message}</div>
-               </>
-            ) : (
-               <>
-                  <button style={{ ...styles.button, backgroundColor: 'red' }} onClick={() => disconnect()}>
-                     Disconnect
-                  </button>
-               </>
-            )}
-         </div>
-         {account?.isConnected && (
-            <>
-               <div style={styles.info}>
-                  <h2 style={styles.title}>Switch Chain</h2>
-                  <div>{switchChainError?.message}</div>
-                  {chains.map((chain) => (
-                     <button
-                        style={{ ...styles.button, backgroundColor: account.chainId === chain.id ? 'gray' : 'blue' }}
-                        key={chain.id}
-                        disabled={account.chainId === chain.id}
-                        onClick={() => switchChain({ chainId: chain.id })}
-                     >
-                        Switch To {chain.name}
-                     </button>
-                  ))}
-               </div>
+    await signMessageAsync({
+      message: 'Hello, world!',
+    });
+  };
 
-               <div style={styles.info}>
-                  <h2 style={styles.title}>Send Transaction</h2>
-                  <div>{txError?.message}</div>
-                  <button
-                     style={{
-                        ...styles.button,
-                        backgroundColor: 'green'
-                     }}
-                     onClick={sendTx}
-                  >
-                     Send Transaction
-                  </button>
-               </div>
+  return (
+    <div style={styles.container}>
+      <div style={styles.info}>
+        <h2 style={styles.title}>Account</h2>
+        <div>
+          <br />
+          Address: {account?.address}
+          <br />
+          ChainId: {account.chainId}
+          <br />
+          Current Chain: {account.chain?.name}
+          <br />
+          <span
+            style={{
+              color: account?.isConnected ? 'green' : 'red',
+              fontWeight: 'bold',
+            }}
+          >
+            Status: {account?.isConnected ? 'Connected' : 'Disconnected'}
+          </span>
+        </div>
+        {!account?.isConnected ? (
+          <>
+            <button
+              style={{ ...styles.button, backgroundColor: 'orange' }}
+              key={connectors[0].id}
+              onClick={() =>
+                connect({
+                  connector: connectors[0],
+                })
+              }
+            >
+              Connect To {connectors[0].name}
+            </button>
 
-               <div style={styles.info}>
-                  <h2 style={styles.title}>Sign Message</h2>
-                  <div>{signError?.message}</div>
-                  <button style={{ ...styles.button, backgroundColor: 'purple' }} onClick={signAMessage}>
-                     Sign Message
-                  </button>
-               </div>
-            </>
-         )}
+            <button
+              style={{ ...styles.button, backgroundColor: 'purple' }}
+              onClick={() => connectAndSign()}
+            >
+              Connect And Sign
+            </button>
+
+            <div>{connectError?.message}</div>
+          </>
+        ) : (
+          <>
+            <button
+              style={{ ...styles.button, backgroundColor: 'red' }}
+              onClick={() => disconnect()}
+            >
+              Disconnect
+            </button>
+          </>
+        )}
       </div>
-   );
+      {account?.isConnected && (
+        <>
+          <div style={styles.info}>
+            <h2 style={styles.title}>Switch Chain</h2>
+            <div>{switchChainError?.message}</div>
+            {chains.map((chain) => (
+              <button
+                style={{
+                  ...styles.button,
+                  backgroundColor:
+                    account.chainId === chain.id ? 'gray' : 'blue',
+                }}
+                key={chain.id}
+                disabled={account.chainId === chain.id}
+                onClick={() => switchChain({ chainId: chain.id })}
+              >
+                Switch To {chain.name}
+              </button>
+            ))}
+          </div>
+
+          <div style={styles.info}>
+            <h2 style={styles.title}>Send Transaction</h2>
+            <div>{txError?.message}</div>
+            <button
+              style={{
+                ...styles.button,
+                backgroundColor: 'green',
+              }}
+              onClick={sendTx}
+            >
+              Send Transaction
+            </button>
+          </div>
+
+          <div style={styles.info}>
+            <h2 style={styles.title}>Sign Message</h2>
+            <div>{signError?.message}</div>
+            <button
+              style={{ ...styles.button, backgroundColor: 'purple' }}
+              onClick={signAMessage}
+            >
+              Sign Message
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 // Inline CSS styles
 const styles = {
-   container: {
-      padding: '20px',
-      fontFamily: 'Arial, sans-serif'
-   },
-   button: {
-      margin: '5px',
-      padding: '15px',
-      color: 'white',
-      border: 'none',
-      borderRadius: '10px',
-      cursor: 'pointer',
-      width: '50%',
-      fontSize: '16px',
-      fontWeight: 'bold'
-   },
-   info: {
-      marginBottom: '20px',
-      padding: '10px',
-      border: '1px solid #ddd',
-      borderRadius: '15px',
-      justifyContent: 'center',
-      alignItems: 'center',
-      textAlign: 'center' as 'center',
-      width: '100%'
-   },
-   title: {
-      marginTop: '0'
-   }
+  container: {
+    padding: '20px',
+    fontFamily: 'Arial, sans-serif',
+  },
+  button: {
+    margin: '5px',
+    padding: '15px',
+    color: 'white',
+    border: 'none',
+    borderRadius: '10px',
+    cursor: 'pointer',
+    width: '50%',
+    fontSize: '16px',
+    fontWeight: 'bold',
+  },
+  info: {
+    marginBottom: '20px',
+    padding: '10px',
+    border: '1px solid #ddd',
+    borderRadius: '15px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center' as 'center',
+    width: '100%',
+  },
+  title: {
+    marginTop: '0',
+  },
 };
 
 export default App;
--- packages/examples/wagmi-demo/src/wagmi.ts
+++ packages/examples/wagmi-demo/src/wagmi.ts
@@ -3,17 +3,25 @@ import { mainnet, polygon } from 'wagmi/chains';
 import { metaMask } from 'wagmi/connectors';
 
 export const config = createConfig({
-   chains: [mainnet, polygon],
-   connectors: [metaMask({})],
-   ssr: true,
-   transports: {
-      [mainnet.id]: http(),
-      [polygon.id]: http()
-   }
+  chains: [mainnet, polygon],
+  connectors: [
+    metaMask({
+      infuraAPIKey: process.env.INFURA_API_KEY,
+      dappMetadata: {
+        name: 'Wagmi-Demo',
+        url: 'https://wagmi.io',
+      },
+    }),
+  ],
+  ssr: true,
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+  },
 });
 
 declare module 'wagmi' {
-   interface Register {
-      config: typeof config;
-   }
+  interface Register {
+    config: typeof config;
+  }
 }
