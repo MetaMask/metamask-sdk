285d158391a2ed1f06d46d5a143b046fd47856fb
Omri Dan <61094771+omridan159@users.noreply.github.com>
2024-02-06 20:09:20 +0200

    feat: create E2E test for Android SDK Dapp (#670)
    * feat: wip
    
    * chore: add more logic to the Android SDK e2e test
    
    * chore: wip
    
    * chore: wip
    
    * chore: remove timers

--- e2e/test/configs/local/wdio.ios.app.local.conf.ts
+++ e2e/test/configs/local/wdio.ios.app.local.conf.ts
@@ -20,14 +20,18 @@ config.capabilities = [
     platformName: 'iOS',
     maxInstances: 1,
     // http://appium.io/docs/en/writing-running-appium/caps/
-    'appium:deviceName': process.env.DEVICE_NAME,
-    'appium:platformVersion': process.env.PLATFORM_VERSION,
-    'appium:automationName': process.env.AUTOMATION_NAME,
-    'appium:app': process.env.APP_PATH,
+    'appium:deviceName': process.env.DEVICE_NAME ?? '',
+    'appium:platformVersion': process.env.PLATFORM_VERSION ?? '',
+    'appium:automationName': process.env.AUTOMATION_NAME ?? '',
+    'appium:app': process.env.APP_PATH ?? '',
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    'appium:bundleId': process.env.BUNDLE_ID,
-    'appium:udid': process.env.DEVICE_UDID,
+    'appium:bundleId': process.env.BUNDLE_ID ?? '',
+    'appium:udid': process.env.DEVICE_UDID ?? '',
+    'appium:otherApps': [
+      process.env.ANDROID_SDK_TEST_APP_PATH ?? '',
+      process.env.RN_TEST_APP_PATH ?? '',
+    ],
     // "appium:xcodeSigningId": "iPhone Developer",
     'appium:newCommandTimeout': 240,
     'appium:noReset': false,
