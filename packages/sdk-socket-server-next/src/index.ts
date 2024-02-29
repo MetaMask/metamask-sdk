2a673e65c41011ac94e972df77794cc97199dacf
abretonc7s <107169956+abretonc7s@users.noreply.github.com>
2024-02-29 17:01:00 +0800

    feat: socket io debug setup (#717)
    * feat: socket io logging and admin ui
    
    * feat: dynamic enablment of rate limiter
    
    * feat: socketio config
    
    * chore: cleanup

--- packages/sdk-socket-server-next/src/index.ts
+++ packages/sdk-socket-server-next/src/index.ts
@@ -5,30 +5,41 @@ import dotenv from 'dotenv';
 // Dotenv must be loaded before importing local files
 dotenv.config();
 
+import { instrument } from '@socket.io/admin-ui';
 import { analytics, app } from './api-config';
 import { logger } from './logger';
-import { cleanupAndExit } from './utils';
-import { configureSocketServer } from './socket-config';
 import { extractMetrics } from './metrics';
+import { configureSocketServer } from './socket-config';
+import { cleanupAndExit } from './utils';
 
 export const isDevelopment: boolean = process.env.NODE_ENV === 'development';
 export const isDevelopmentServer: boolean =
   process.env.ENVIRONMENT === 'development';
+export const withAdminUI: boolean = process.env.ADMIN_UI === 'true';
 
 const server = http.createServer(app);
 
+// Register event listeners for process termination events
+process.on('SIGINT', async () => {
+  await cleanupAndExit(server, analytics);
+});
+
+process.on('SIGTERM', async () => {
+  await cleanupAndExit(server, analytics);
+});
+
 configureSocketServer(server)
   .then((ioServer) => {
-    logger.info('INFO> isDevelopment?', isDevelopment);
+    logger.info(
+      `socker.io server started development=${isDevelopment} adminUI=${withAdminUI}`,
+    );
 
-    // Register event listeners for process termination events
-    process.on('SIGINT', async () => {
-      await cleanupAndExit(server, analytics);
-    });
-
-    process.on('SIGTERM', async () => {
-      await cleanupAndExit(server, analytics);
-    });
+    if (isDevelopmentServer && withAdminUI) {
+      instrument(ioServer, {
+        auth: false,
+        mode: 'development',
+      });
+    }
 
     // Make sure to protect the endpoint to be only available within the cluster for prometheus
     app.get('/metrics', async (_req, res) => {
