7bad773befe38d0caf474023220e57798192499c
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-03-08 09:35:41 +0000

    fix: properly declares chains constants

--- packages/examples/nodejs-interactive/src/constants.ts
+++ packages/examples/nodejs-interactive/src/constants.ts
@@ -11,33 +11,34 @@ export interface Chain {
   rpcUrls: string[];
 }
 
-export const chains = {
+export const chains: {[name: string]: Chain} = {
   GOERLI: {
     chainId: '0x5',
     chainName: 'Goerli',
     blockExplorerUrls: ['https://goerli.etherscan.io'],
     nativeCurrency: { symbol: 'ETH', decimals: 18 },
     rpcUrls: ['https://goerli.infura.io/v3/'],
-  } as Chain,
+  },
   SEPOLIA: {
     chainId: '0xaa36a7',
     chainName: 'Sepolia',
     blockExplorerUrls: ['https://sepolia.etherscan.io'],
     nativeCurrency: { symbol: 'ETH', decimals: 18 },
     rpcUrls: ['https://sepolia.infura.io/v3/'],
-  } as Chain,
+  },
   POLYGON: {
     chainId: '0x89',
     chainName: 'Polygon Mainnet',
     blockExplorerUrls: ['https://polygonscan.com'],
     nativeCurrency: { symbol: 'MATIC', decimals: 18 },
     rpcUrls: ['https://polygon-rpc.com/'],
-  } as Chain,
+  },
   MAINNET: {
     chainId: '0x1',
     chainName: 'Mainnet',
     blockExplorerUrls: ['https://etherscan.io'],
     nativeCurrency: { symbol: 'ETH', decimals: 18 },
     rpcUrls: ['https://mainnet.infura.io/v3/'],
-  } as Chain,
+  },
 }
+
