febcb53aeeab66e6334da63c6d8827d6919f091d
abretonc7s <arthur.breton@consensys.net>
2024-04-16 21:45:44 +0800

    feat: preferDesktop select correct tab

--- packages/sdk/src/sdk.ts
+++ packages/sdk/src/sdk.ts
@@ -75,7 +75,6 @@ export interface MetaMaskSDKOptions {
 
   /**
    * If true, the SDK will prefer the desktop version of MetaMask over the mobile version.
-   * @deprecated use `extensionOnly` instead
    */
   preferDesktop?: boolean;
 
@@ -260,13 +259,6 @@ export class MetaMaskSDK extends EventEmitter2 {
         throw new Error(`Invalid defaultReadOnlyChainId, must start with '0x'`);
       }
       this.defaultReadOnlyChainId = options.defaultReadOnlyChainId.toString();
-    }
-
-    // Remove deprecated options
-    if (options.preferDesktop) {
-      console.warn(
-        `The 'preferDesktop' option is deprecated. Use 'extensionOnly' instead.`,
-      );
     }
 
     this.options = options;
