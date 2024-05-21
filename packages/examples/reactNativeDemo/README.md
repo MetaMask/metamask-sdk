29ef3ec0e1ad6ef325337b1c709ced0bfa06381a
Omridan159 <omridan159@gmail.com>
2024-05-21 17:08:00 +0300

    chore: recreate a reactNativeDemo test dapp

---
+++ packages/examples/reactNativeDemo/README.md
@@ -0,0 +1,3 @@
+# React Native SDK example
+
+We recommend using React Native v0.71.4 or higher to avoid performance issues on Android.
---
+++ packages/examples/reactNativeDemo/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unassigned-import */
+/**
+ * @format
+ */
+import 'node-libs-react-native/globals';
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
+
+import {AppRegistry} from 'react-native';
+import {SafeApp} from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => SafeApp);
