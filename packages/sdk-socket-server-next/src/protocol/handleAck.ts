d094539f86131edfd20f1066f2bd29f3f3e5075e
abretonc7s <arthur.breton@consensys.net>
2024-06-06 17:24:58 +0800

    feat: add channelId to each log for easier tracking

--- packages/sdk-socket-server-next/src/protocol/handleAck.ts
+++ packages/sdk-socket-server-next/src/protocol/handleAck.ts
@@ -24,28 +24,38 @@ export const handleAck = async ({
     // Retrieve all messages to find and remove the specified one
     messages = await pubClient.lrange(queueKey, 0, -1);
     logger.debug(
-      `Messages in ${clientType} queue: ${messages.length}`,
+      `handleAck channelId=${channelId} -- Messages in ${clientType} queue: ${messages.length}`,
       messages,
     );
     const index = messages.findIndex((msg) => {
       try {
         const parsed = JSON.parse(msg) as QueuedMessage;
-        logger.debug(`Parsed ackId: ${parsed.ackId}, Target ackId: ${ackId}`);
+        // logger.debug(`Parsed ackId: ${parsed.ackId}, Target ackId: ${ackId}`);
         return parsed.ackId === ackId;
       } catch (e) {
-        logger.error('Error parsing message', msg, e);
+        logger.error(
+          `handleAck channelId=${channelId} -- Error parsing message`,
+          msg,
+          e,
+        );
         return false;
       }
     });
     if (index === -1) {
-      logger.warn(`Message ${ackId} not found in ${clientType} queue.`);
+      logger.warn(
+        `handleAck channelId=${channelId} -- Message ${ackId} not found in ${clientType} queue.`,
+      );
     } else {
       const placeholder = `TO_REMOVE_${new Date().getTime()}`; // Unique placeholder
       await pubClient.lset(queueKey, index, placeholder); // Set the message at index to unique placeholder
       await pubClient.lrem(queueKey, 1, placeholder); // Remove the unique placeholder
-      logger.info(`Message ${ackId} removed from ${clientType} queue.`);
+      logger.info(
+        `handleAck channelId=${channelId} -- Message ${ackId} removed from ${clientType} queue.`,
+      );
     }
   } catch (error) {
-    logger.error(`Error removing message: ${error}`);
+    logger.error(
+      `handleAck channelId=${channelId} -- Error removing message: ${error}`,
+    );
   }
 };
