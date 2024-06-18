04cae6257339acd14402bed9f5e1a3533a57d5e2
Omridan159 <omridan159@gmail.com>
2024-06-18 17:14:53 +0300

    chore: add unit tests

--- packages/sdk/src/services/rpc-requests/RPCRequestHandler.test.ts
+++ packages/sdk/src/services/rpc-requests/RPCRequestHandler.test.ts
@@ -16,7 +16,8 @@ jest.mock('./RPCRequestHandler', () => {
 
 interface MockResponse {
   ok: boolean;
-  json: jest.Mock<Promise<{ someKey: string }>>;
+  json: jest.Mock<Promise<unknown>>;
+  status: number;
 }
 
 describe('rpcRequestHandler', () => {
@@ -33,6 +34,7 @@ describe('rpcRequestHandler', () => {
         jsonrpc: '2.0',
         result: { someKey: 'someValue' },
       }),
+      status: 200,
     };
 
     (crossFetch as jest.MockedFunction<typeof crossFetch>).mockResolvedValue(
@@ -54,5 +56,78 @@ describe('rpcRequestHandler', () => {
     expect(result).toStrictEqual({ someKey: 'someValue' });
   });
 
-  // Add other test cases as required, such as error handling, etc.
+  it('should include Metamask-Sdk-Info header if endpoint includes "infura"', async () => {
+    const params = {
+      rpcEndpoint: 'https://infura.io/test-endpoint',
+      sdkInfo: 'testSDK',
+      method: 'testMethod',
+      params: ['param1', 'param2'],
+    };
+
+    await rpcRequestHandler(params);
+
+    // Check if crossFetch was called with the correct headers
+    expect(crossFetch).toHaveBeenCalledWith(
+      'https://infura.io/test-endpoint',
+      expect.objectContaining({
+        headers: expect.objectContaining({
+          'Metamask-Sdk-Info': 'testSDK',
+        }),
+      }),
+    );
+  });
+
+  it('should not include Metamask-Sdk-Info header if endpoint does not include "infura"', async () => {
+    const params = {
+      rpcEndpoint: 'https://test-endpoint.com',
+      sdkInfo: 'testSDK',
+      method: 'testMethod',
+      params: ['param1', 'param2'],
+    };
+
+    await rpcRequestHandler(params);
+
+    // Check if crossFetch was called with the correct headers
+    expect(crossFetch).toHaveBeenCalledWith(
+      'https://test-endpoint.com',
+      expect.objectContaining({
+        headers: expect.not.objectContaining({
+          'Metamask-Sdk-Info': 'testSDK',
+        }),
+      }),
+    );
+  });
+
+  it('should throw an error if fetch fails', async () => {
+    (
+      crossFetch as jest.MockedFunction<typeof crossFetch>
+    ).mockRejectedValueOnce(new Error('Fetch error'));
+
+    const params = {
+      rpcEndpoint: 'https://test-endpoint.com',
+      sdkInfo: 'testSDK',
+      method: 'testMethod',
+      params: ['param1', 'param2'],
+    };
+
+    await expect(rpcRequestHandler(params)).rejects.toThrow(
+      'Failed to fetch from RPC: Fetch error',
+    );
+  });
+
+  it('should throw an error if response is not ok', async () => {
+    mockResponse.ok = false;
+    mockResponse.status = 500;
+
+    const params = {
+      rpcEndpoint: 'https://test-endpoint.com',
+      sdkInfo: 'testSDK',
+      method: 'testMethod',
+      params: ['param1', 'param2'],
+    };
+
+    await expect(rpcRequestHandler(params)).rejects.toThrow(
+      'Server responded with a status of 500',
+    );
+  });
 });
--- packages/sdk/src/storage-manager/StorageManagerWeb.test.ts
+++ packages/sdk/src/storage-manager/StorageManagerWeb.test.ts
@@ -1,5 +1,9 @@
 import { ChannelConfig } from '@metamask/sdk-communication-layer';
-import { STORAGE_PATH } from '../config'; // Update the import to match the correct location
+import {
+  STORAGE_DAPP_CHAINID,
+  STORAGE_DAPP_SELECTED_ADDRESS,
+  STORAGE_PATH,
+} from '../config';
 import { StorageManagerWeb } from './StorageManagerWeb';
 
 describe('StorageManagerWeb', () => {
@@ -57,12 +61,109 @@ describe('StorageManagerWeb', () => {
       const result = await storageManager.getPersistedChannelConfig();
       expect(result).toBeUndefined();
     });
+
+    it('should handle errors gracefully', async () => {
+      mockLocalStorage.getItem.mockImplementationOnce(() => {
+        throw new Error('Error reading localStorage');
+      });
+
+      const result = await storageManager.getPersistedChannelConfig();
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('persistAccounts', () => {
+    it('should store the accounts in localStorage', async () => {
+      const accounts = ['0xABC'];
+
+      await storageManager.persistAccounts(accounts);
+      expect(localStorage.setItem).toHaveBeenCalledWith(
+        STORAGE_DAPP_SELECTED_ADDRESS,
+        JSON.stringify(accounts),
+      );
+    });
+  });
+
+  describe('getCachedAccounts', () => {
+    it('should return the stored accounts if they exist', async () => {
+      const accounts = ['0xABC'];
+      mockLocalStorage.getItem.mockReturnValueOnce(JSON.stringify(accounts));
+
+      const result = await storageManager.getCachedAccounts();
+      expect(result).toStrictEqual(accounts);
+    });
+
+    it('should return an empty array if no accounts exist in localStorage', async () => {
+      mockLocalStorage.getItem.mockReturnValueOnce(null);
+
+      const result = await storageManager.getCachedAccounts();
+      expect(result).toStrictEqual([]);
+    });
+
+    it('should handle errors when reading accounts', async () => {
+      mockLocalStorage.getItem.mockImplementationOnce(() => {
+        throw new Error('Error reading localStorage');
+      });
+
+      await expect(storageManager.getCachedAccounts()).rejects.toThrow(
+        'Error reading localStorage',
+      );
+    });
+  });
+
+  describe('persistChainId', () => {
+    it('should store the chainId in localStorage', async () => {
+      const chainId = '0x1';
+
+      await storageManager.persistChainId(chainId);
+      expect(localStorage.setItem).toHaveBeenCalledWith(
+        STORAGE_DAPP_CHAINID,
+        chainId,
+      );
+    });
+  });
+
+  describe('getCachedChainId', () => {
+    it('should return the stored chainId if it exists', async () => {
+      const chainId = '0x1';
+      mockLocalStorage.getItem.mockReturnValueOnce(chainId);
+
+      const result = await storageManager.getCachedChainId();
+      expect(result).toStrictEqual(chainId);
+    });
+
+    it('should return undefined if no chainId exists in localStorage', async () => {
+      mockLocalStorage.getItem.mockReturnValueOnce(null);
+
+      const result = await storageManager.getCachedChainId();
+      expect(result).toBeUndefined();
+    });
+
+    it('should handle errors when reading chainId', async () => {
+      mockLocalStorage.getItem.mockImplementationOnce(() => {
+        throw new Error('Error reading localStorage');
+      });
+
+      await expect(storageManager.getCachedChainId()).rejects.toThrow(
+        'Error reading localStorage',
+      );
+    });
   });
 
   describe('terminate', () => {
     it('should remove the stored channel config from localStorage', async () => {
       await storageManager.terminate();
       expect(localStorage.removeItem).toHaveBeenCalledWith(STORAGE_PATH);
+    });
+
+    it('should remove the stored accounts from localStorage', async () => {
+      await storageManager.terminate();
+      expect(localStorage.removeItem).toHaveBeenCalledWith('.sdk-comm');
+    });
+
+    it('should remove the stored chainId from localStorage', async () => {
+      await storageManager.terminate();
+      expect(localStorage.removeItem).toHaveBeenCalledWith('.sdk-comm');
     });
   });
 });
