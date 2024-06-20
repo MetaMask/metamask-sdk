15868a3e82a6bfa8826fc8ddb04d740ebc2b1c2f
abretonc7s <arthur.breton@consensys.net>
2024-06-20 11:47:40 +0800

    feat: add devexpo project

--- packages/devexpo/metro.config.js
+++ packages/devexpo/metro.config.js
@@ -2,6 +2,12 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
+// eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+config.resolver = {
+  extraNodeModules: {
+    ...require('node-libs-react-native'),
+  },
+};
 
 module.exports = config;
