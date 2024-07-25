410ec5a3b97afcf737d550894eba702b0be59037
Omri Dan <61094771+omridan159@users.noreply.github.com>
2024-07-25 18:20:09 +0400

    fix: devnext compiling issues (#965)

--- packages/devnext/babel.config.js
+++ packages/devnext/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['next/babel', 'babel-preset-expo'],
-    plugins: ['@babel/plugin-proposal-export-namespace-from', 'react-native-reanimated/plugin']
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
+      ["react-native-web", { commonjs: true }]
+    ]
   };
 };
