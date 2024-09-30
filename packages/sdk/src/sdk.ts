a522b6d023dcf4d5bc737c5f9a05badea5c02177
abretonc7s <arthur.breton@consensys.net>
2024-09-30 15:29:52 +0800

    feat: remove unnecessary option - let wallet send information

--- packages/sdk/src/sdk.ts
+++ packages/sdk/src/sdk.ts
@@ -88,11 +88,6 @@ export interface MetaMaskSDKOptions {
   shouldShimWeb3?: boolean;
 
   /**
-   * Enable the experimental deeplink protocol on mobile web.
-   */
-  _experimentalDeeplinkProtocol?: boolean;
-
-  /**
    * The preferred communication layer to use for the SDK.
    */
   communicationLayerPreference?: CommunicationLayerPreference;
