cccb9325fa64311776d6faffa7f03e3a74149186
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-10-21 19:02:36 +0100

    chore: update examples to latest

--- packages/examples/wagmi-demo-react/vite.config.ts
+++ packages/examples/wagmi-demo-react/vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: './build',
+    emptyOutDir: true, // also necessary
+  }
 })
