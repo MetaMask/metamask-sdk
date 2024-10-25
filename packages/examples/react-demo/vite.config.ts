bb3051071a7a83f217cb61f787b23a329ebdadfa
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-10-25 10:35:21 +0100

    fix: fixes vite config

--- packages/examples/react-demo/vite.config.ts
+++ packages/examples/react-demo/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: './build',
     emptyOutDir: true, // also necessary
