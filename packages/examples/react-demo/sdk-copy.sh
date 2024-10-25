3addedd762b2496cafa261e99c5d68d5903177f6
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-10-25 15:25:03 +0100

    fix: fixes sdk_copy to include sdk-react

--- packages/examples/react-demo/sdk-copy.sh
+++ packages/examples/react-demo/sdk-copy.sh
@@ -7,7 +7,6 @@ SDK_WORKSPACE_DIR="$( pwd; )";
 COMM_LAYER_DIR="$SDK_WORKSPACE_DIR/packages/sdk-communication-layer"
 SDK_DIR="$SDK_WORKSPACE_DIR/packages/sdk"
 SDK_REACT_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react"
-SDK_REACT_UI_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react-ui"
 SDK_INSTALL_MODAL_WEB_DIR="$SDK_WORKSPACE_DIR/packages/sdk-install-modal-web"
 
 DAPP_DIR="$SDK_WORKSPACE_DIR/packages/examples/react-demo"
@@ -24,12 +23,15 @@ echo "########### START REPLACING SDK_COMMUNICATION_LAYER #########"
 cd $DAPP_DIR
 echo "Hack Metamask sdk && sdk-communication-layer packages..."
 ## hack to debug to latest unpublished version of the sdk
-rm -rf node_modules/@metamask/sdk-communication-layer/dist node_modules/@metamask/sdk/dist node_modules/@metamask/sdk-react/dist node_modules/@metamask/sdk-react-ui/dist
+rm -rf node_modules/@metamask/sdk-communication-layer/dist node_modules/@metamask/sdk/dist node_modules/@metamask/sdk-react/dist
 cp -rf $COMM_LAYER_DIR/dist node_modules/@metamask/sdk-communication-layer/dist
 cp -rf $COMM_LAYER_DIR/package.json node_modules/@metamask/sdk-communication-layer/package.json
 
 cp -rf $SDK_DIR/dist node_modules/@metamask/sdk/dist
 cp -rf $SDK_DIR/package.json node_modules/@metamask/sdk/package.json
+
+cp -rf $SDK_REACT_DIR/dist node_modules/@metamask/sdk-react/dist
+cp -rf $SDK_REACT_DIR/package.json node_modules/@metamask/sdk-react/package.json
 
 cp -rf $SDK_INSTALL_MODAL_WEB_DIR/dist node_modules/@metamask/sdk-install-modal-web/dist
 cp -rf $SDK_INSTALL_MODAL_WEB_DIR/package.json node_modules/@metamask/sdk-install-modal-web/package.json
