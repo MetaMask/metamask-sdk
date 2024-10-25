f27dc586d092d7ed4638756bd76de0bb3bb0c114
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-10-25 15:23:31 +0100

    fix: fixes sdk_copy

--- packages/examples/react-demo/sdk-copy.sh
+++ packages/examples/react-demo/sdk-copy.sh
@@ -8,6 +8,7 @@ COMM_LAYER_DIR="$SDK_WORKSPACE_DIR/packages/sdk-communication-layer"
 SDK_DIR="$SDK_WORKSPACE_DIR/packages/sdk"
 SDK_REACT_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react"
 SDK_REACT_UI_DIR="$SDK_WORKSPACE_DIR/packages/sdk-react-ui"
+SDK_INSTALL_MODAL_WEB_DIR="$SDK_WORKSPACE_DIR/packages/sdk-install-modal-web"
 
 DAPP_DIR="$SDK_WORKSPACE_DIR/packages/examples/react-demo"
 
@@ -16,6 +17,7 @@ echo "COMM_LAYER_DIR: $COMM_LAYER_DIR"
 echo "DAPP_DIR: $DAPP_DIR"
 echo "SDK_REACT_DIR: $SDK_REACT_DIR"
 echo "SDK_REACT_UI_DIR: $SDK_REACT_UI_DIR"
+echo "SDK_INSTALL_MODAL_WEB_DIR: $SDK_INSTALL_MODAL_WEB_DIR"
 
 echo "########### START REPLACING SDK_COMMUNICATION_LAYER #########"
 
