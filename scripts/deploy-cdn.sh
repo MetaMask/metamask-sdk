d0ae115b621a728c491443e3e3d07f0c428b36ca
Christopher Ferreira <christopher.ferreira@consensys.net>
2024-11-12 11:29:18 +0000

    feat: adds CDN deployments to gh pages

---
+++ scripts/deploy-cdn.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Stop on first error
+set -e
+
+# Make sure to start from base workspace folder
+reldir="$( dirname -- "$0"; )";
+cd "$reldir/..";
+
+build_sdk() {
+    echo "\n---------- Building SDK -------------\n"
+    yarn build
+    echo "\n---------- Done building SDK -------------\n"
+}
+
+copy_to_deployment_dir() {
+    local deployment_dir=$1
+    echo "\n---------- Copying to deployment directory -------------\n"
+    cp -r packages/sdk/dist/browser/iife/metamask-sdk.js "$deployment_dir"
+    cp -r packages/sdk/dist/browser/iife/metamask-sdk.js.map "$deployment_dir"
+    echo "\n---------- Done copying to deployment directory -------------\n"
+}
+
+# ------ Start
+deployment_folder="cdn"
+deployment_dir="deployments/$deployment_folder"
+
+echo "Deployment folder: $deployment_folder"
+echo "Deployment directory: $deployment_dir"
+
+mkdir -p "$deployment_dir"
+
+# Build SDK from root
+build_sdk
+
+# Copy to deployment directory
+copy_to_deployment_dir "$deployment_dir"
