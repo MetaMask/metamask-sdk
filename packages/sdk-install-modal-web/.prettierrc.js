79bc266fc12835ba01557aee36e7c3cea2902012
Edward Penta <eddie.penta@consensys.net>
2024-11-12 10:58:52 -0500

    chore: re-add eslint and prettier configs

---
+++ packages/sdk-install-modal-web/.prettierrc.js
@@ -0,0 +1,15 @@
+// All of these are defaults except singleQuote, but we specify them
+// for explicitness
+module.exports = {
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 180,
+  semi: true,
+  useTabs: false
+};
