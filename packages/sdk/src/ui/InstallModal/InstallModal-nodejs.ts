40f6ae6178d24676216683cbdb935812ff7ef898
Edward Penta <eddie.penta@consensys.net>
2024-11-14 21:24:06 -0500

    chore: clean up

--- packages/sdk/src/ui/InstallModal/InstallModal-nodejs.ts
+++ packages/sdk/src/ui/InstallModal/InstallModal-nodejs.ts
@@ -1,9 +1,9 @@
+import encodeQR from '@paulmillr/qr';
 import { logger } from '../../utils/logger';
-import encodeQR from "@paulmillr/qr"
 
 const InstallModal = ({ link }: { link: string; debug?: boolean }) => {
-  const qr = encodeQR(link, 'ascii')
-  console.log(qr)
+  const qr = encodeQR(link, 'ascii');
+  console.log(qr);
   logger(`[UI: InstallModal-nodejs()] qrcode url: ${link}`);
   return {
     unmount: () => undefined,
