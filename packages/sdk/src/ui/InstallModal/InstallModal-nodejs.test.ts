7cb7c11b1e1264fa7d1cd80968d9f8b161b5ed82
Edward Penta <eddie.penta@consensys.net>
2024-11-19 10:43:11 -0500

    chore: lint

--- packages/sdk/src/ui/InstallModal/InstallModal-nodejs.test.ts
+++ packages/sdk/src/ui/InstallModal/InstallModal-nodejs.test.ts
@@ -2,7 +2,7 @@ import encodeQR from '@paulmillr/qr';
 import * as loggerModule from '../../utils/logger';
 import InstallModal from './InstallModal-nodejs';
 
-jest.mock('@paulmillr/qr', () => (jest.fn((_, __) => 'Mocked QR Code')));
+jest.mock('@paulmillr/qr', () => jest.fn((_, __) => 'Mocked QR Code'));
 
 describe('InstallModal-nodejs', () => {
   const spyLogger = jest.spyOn(loggerModule, 'logger');
@@ -25,10 +25,7 @@ describe('InstallModal-nodejs', () => {
 
     InstallModal({ link: mockLink });
 
-    expect(mockGenerate).toHaveBeenCalledWith(
-      mockLink,
-      'ascii'
-    );
+    expect(mockGenerate).toHaveBeenCalledWith(mockLink, 'ascii');
   });
 
   it('should log the QR code link', () => {
