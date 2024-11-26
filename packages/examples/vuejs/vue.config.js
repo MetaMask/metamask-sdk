8ab1cf1190eab2e8efa6baa4c83c59f701df29f3
Christopher Ferreira <104831203+christopherferreira9@users.noreply.github.com>
2024-11-26 10:50:16 +0000

    fix(vuejs): vuejs example (#1146)
    * feat: release 103.0.0
    
    * docs: update changelogs
    
    * fix: fixes the vuejs build example for 0.31.0
    
    ---------
    
    Co-authored-by: abretonc7s <arthur.breton@consensys.net>

--- packages/examples/vuejs/vue.config.js
+++ packages/examples/vuejs/vue.config.js
@@ -2,4 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
+  chainWebpack: (config) => {
+    config.module
+      .rule('source-map')
+      .test(/\.map$/)
+      .use('source-map-loader')
+      .loader('source-map-loader')
+      .end()
+      .enforce('pre');
+
+    config.module
+      .rule('ts-declaration')
+      .test(/\.d\.ts$/)
+      .use('null-loader')
+      .loader('null-loader');
+  }
 })
