d57e5da2aeedf8f522ea76ada35ea059ce359921
abretonc7s <arthur.breton@consensys.net>
2025-02-13 14:55:10 +0800

    feat: nav header

--- packages/playground-next/src/pages/_app.tsx
+++ packages/playground-next/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'; // Import global CSS here
 import type { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
