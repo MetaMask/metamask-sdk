47a41b73ec155dda43a83dd954e7c251f4d9489f
Christopher Ferreira <christopher.ferreira@consensys.net>
2025-04-09 11:34:04 +0100

    chore: adds the wip single user test

--- packages/sdk-performance-tests/k6-performance/src/constants.ts
+++ packages/sdk-performance-tests/k6-performance/src/constants.ts
@@ -1,19 +1,19 @@
 export const PROTOCOL = 'wss://';
 
 export enum socketResponseType {
-    open = 0,
-    close = 1,
-    ping = 2,
-    pong = 3,
-    message = 4,
-    upgrade = 5,
-    noop = 6
+  open = 0,
+  close = 1,
+  ping = 2,
+  pong = 3,
+  message = 4,
+  upgrade = 5,
+  noop = 6,
 }
-  
+
 export enum socketResponseCode {
-    connect = 0,
-    disconnect = 1,
-    event = 2,
-    ack = 3,
-    error = 4
+  connect = 0,
+  disconnect = 1,
+  event = 2,
+  ack = 3,
+  error = 4,
 }
--- packages/sdk-performance-tests/k6-performance/src/index.ts
+++ packages/sdk-performance-tests/k6-performance/src/index.ts
@@ -1,4 +1,28 @@
-import { generateChannelIds } from "./helpers";
+import { randomUUID } from 'node:crypto';
+import fs from 'node:fs';
+
+type ChannelID = { channelId: string };
+type UUIDs = { uuids: ChannelID[] };
+
+export const generateChannelIds = () => {
+  console.log('Generating channel ids...');
+  const roomIds: UUIDs = {
+    uuids: [],
+  };
+  // Generate 1,000,000 channel ids
+  for (let i = 0; i < 1_000_000; i++) {
+    roomIds.uuids.push({
+      channelId: randomUUID(),
+    });
+  }
+
+  fs.writeFile('socketioUUID.json', JSON.stringify(roomIds), (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+  console.log('Channel ids generated.');
+};
 
 // Generating channel ids...
 generateChannelIds();
