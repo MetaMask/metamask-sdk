75eddd2632e007d9eefd3409c3b6d8fa66653a92
Arthur Breton <arthur.breton@consensys.net>
2025-04-15 20:33:04 +0800

    feat(tests): enhance test coverage and refactor global mocks
    - Updated Jest configuration to include module name mappings for analytics and SDK types.
    - Refactored test files to utilize the new `@metamask/analytics-client` package for analytics handling.
    - Improved global mock setup for `navigator` and `window` properties in tests to ensure proper environment simulation.
    - Added cleanup logic in tests to restore original global properties after each test execution.
    - Enhanced test cases for `PlatformManager` and `isOldIOS` to ensure accurate platform detection and behavior.

--- packages/sdk-communication-layer/src/services/RemoteCommunication/EventListeners/handleClientsConnectedEvent.test.ts
+++ packages/sdk-communication-layer/src/services/RemoteCommunication/EventListeners/handleClientsConnectedEvent.test.ts
@@ -1,12 +1,12 @@
-import { RemoteCommunication } from '../../../RemoteCommunication';
-import { CommunicationLayerPreference } from '../../../types/CommunicationLayerPreference';
-import { EventType } from '../../../types/EventType';
-import { SendAnalytics } from '../../../Analytics';
+import { SendAnalytics } from '@metamask/analytics-client';
+import { TrackingEvents } from '@metamask/sdk-types';
 import packageJson from '../../../../package.json';
+import { RemoteCommunication } from '../../../RemoteCommunication';
+import { EventType } from '../../../types/EventType';
 import { logger } from '../../../utils/logger';
 import { handleClientsConnectedEvent } from './handleClientsConnectedEvent';
 
-jest.mock('../../../Analytics', () => ({
+jest.mock('@metamask/analytics-client', () => ({
   SendAnalytics: jest.fn().mockResolvedValue(undefined),
 }));
 
@@ -25,16 +25,19 @@ describe('handleClientsConnectedEvent', () => {
     instance = {
       state: {
         debug: true,
+        context: 'mockContext',
         channelId: 'testChannel',
         analytics: true,
+        sdkVersion: '1.0',
+        walletInfo: { version: '1.0' },
         communicationLayer: {
           getKeyInfo: mockGetKeyInfo,
         },
-        sdkVersion: '1.0',
         originatorInfo: {
           someKey: 'someValue',
         },
-        communicationServerUrl: 'http://mock-url.com',
+        analyticsServerUrl: 'http://mock-url.com',
+        isOriginator: false,
       },
       emit: mockEmit,
     } as unknown as RemoteCommunication;
@@ -45,10 +48,7 @@ describe('handleClientsConnectedEvent', () => {
   });
 
   it('should log the event details', () => {
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     handler();
 
     expect(spyLogger).toHaveBeenCalledWith(
@@ -57,37 +57,30 @@ describe('handleClientsConnectedEvent', () => {
   });
 
   it('should send analytics data if analytics tracking is enabled', async () => {
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     await handler();
     expect(SendAnalytics).toHaveBeenCalledWith(
       expect.objectContaining({
+        event: TrackingEvents.RECONNECT,
         id: 'testChannel',
-        commLayer: CommunicationLayerPreference.SOCKET,
         sdkVersion: '1.0',
         commLayerVersion: packageJson.version,
+        walletVersion: '1.0',
+        someKey: 'someValue',
       }),
       'http://mock-url.com',
     );
   });
 
   it('should update the state of the instance correctly', () => {
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     handler();
     expect(instance.state.clientsConnected).toBe(true);
     expect(instance.state.originatorInfoSent).toBe(false);
   });
 
   it('should emit CLIENTS_CONNECTED event', () => {
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     handler();
     expect(mockEmit).toHaveBeenCalledWith(EventType.CLIENTS_CONNECTED);
   });
@@ -96,10 +89,7 @@ describe('handleClientsConnectedEvent', () => {
     (SendAnalytics as jest.Mock).mockRejectedValueOnce(
       new Error('Failed to send analytics'),
     );
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     await handler();
     expect(console.error).toHaveBeenCalledWith(
       'Cannot send analytics',
@@ -109,33 +99,21 @@ describe('handleClientsConnectedEvent', () => {
 
   it('should not send analytics data if analytics tracking is disabled', async () => {
     instance.state.analytics = false;
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     await handler();
     expect(SendAnalytics).not.toHaveBeenCalled();
   });
 
   it('should handle missing channelId gracefully', async () => {
     delete instance.state.channelId;
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     await handler();
-    expect(SendAnalytics).toHaveBeenCalledWith(
-      expect.objectContaining({ id: '' }),
-      'http://mock-url.com',
-    );
+    expect(SendAnalytics).not.toHaveBeenCalled();
   });
 
   it('should handle missing communicationLayer gracefully', () => {
     delete instance.state.communicationLayer;
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     handler();
     // No errors should be thrown
     expect(mockEmit).toHaveBeenCalledWith(EventType.CLIENTS_CONNECTED);
@@ -143,18 +121,20 @@ describe('handleClientsConnectedEvent', () => {
 
   it('should send analytics data correctly if originatorInfo is missing', async () => {
     delete instance.state.originatorInfo;
-    const handler = handleClientsConnectedEvent(
-      instance,
-      CommunicationLayerPreference.SOCKET,
-    );
+    const handler = handleClientsConnectedEvent(instance);
     await handler();
     expect(SendAnalytics).toHaveBeenCalledWith(
       expect.objectContaining({
-        commLayer: CommunicationLayerPreference.SOCKET,
+        event: TrackingEvents.RECONNECT,
+        id: 'testChannel',
         sdkVersion: '1.0',
         commLayerVersion: packageJson.version,
+        walletVersion: '1.0',
       }),
       'http://mock-url.com',
     );
+    const receivedParams = (SendAnalytics as jest.Mock).mock.calls[0][0];
+    expect(receivedParams).not.toHaveProperty('originatorInfo');
+    expect(receivedParams).not.toHaveProperty('someKey');
   });
 });
--- packages/sdk/jest.config.ts
+++ packages/sdk/jest.config.ts
@@ -2,6 +2,16 @@ import baseConfig from '../../jest.config.base';
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    '^@metamask/sdk-types$': '<rootDir>/../sdk-types/src/index.ts',
+    '^@metamask/analytics-client$': '<rootDir>/../analytics-client/src/index.ts',
+    ...(baseConfig.moduleNameMapper || {}),
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@metamask/analytics-client|@metamask/sdk-types)/)',
+    '\\\\.pnp\\\\.[^\\\\/]+$',
+  ],
+  transform: baseConfig.transform,
   coveragePathIgnorePatterns: ['./src/types', '/index.ts$'],
   coverageThreshold: {
     global: {
