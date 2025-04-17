d9f787a7b00c549cf1bb65b732c6aa802fb5e560
Edward Penta <eddie.penta@consensys.net>
2025-04-16 22:29:00 -0400

    feat(analytics): v2 client

---
+++ packages/analytics-client/src/v2/index.ts
@@ -0,0 +1,6 @@
+import GlobalAnalyticsClient from './client';
+
+export { initAnalyticsClient, trackEvent } from './client';
+export type { AnalyticsProps, BaseAnalyticsProps } from './client';
+
+export default GlobalAnalyticsClient;
