6f00ebc57f2c678a2c2cc2a87fc939e23a9d7c38
Christopher Ferreira <104831203+christopherferreira9@users.noreply.github.com>
2025-05-20 10:49:07 +0100

    fix: fixes the build copy script to include sdk-analytics (#1295)

--- packages/examples/vuejs/sdk-copy.sh
+++ packages/examples/vuejs/sdk-copy.sh
@@ -6,23 +6,32 @@ cd "$reldir/../../..";
 SDK_WORKSPACE_DIR="$( pwd; )";
 COMM_LAYER_DIR="$SDK_WORKSPACE_DIR/packages/sdk-communication-layer"
 SDK_DIR="$SDK_WORKSPACE_DIR/packages/sdk"
+SDK_ANALYTICS_DIR="$SDK_WORKSPACE_DIR/packages/sdk-analytics"
 
 DAPP_DIR="$SDK_WORKSPACE_DIR/packages/examples/vuejs"
 
 echo "SDK_DIR: $SDK_DIR"
 echo "COMM_LAYER_DIR: $COMM_LAYER_DIR"
 echo "DAPP_DIR: $DAPP_DIR"
-
+echo "SDK_ANALYTICS_DIR: $SDK_ANALYTICS_DIR"
 echo "########### START REPLACING SDK_COMMUNICATION_LAYER #########"
 
 cd $DAPP_DIR
 echo "Hack Metamask sdk && sdk-communication-layer packages..."
 ## hack to debug to latest unpublished version of the sdk
-rm -rf node_modules/@metamask/sdk-communication-layer/dist node_modules/@metamask/sdk/dist 
+rm -rf node_modules/@metamask/sdk-communication-layer/dist node_modules/@metamask/sdk/dist node_modules/@metamask/sdk-analytics/dist
+mkdir -p node_modules/@metamask/sdk-communication-layer
+mkdir -p node_modules/@metamask/sdk
+mkdir -p node_modules/@metamask/sdk-analytics
+
 cp -rf $COMM_LAYER_DIR/dist node_modules/@metamask/sdk-communication-layer/dist
 cp -rf $COMM_LAYER_DIR/package.json node_modules/@metamask/sdk-communication-layer/package.json
 
 cp -rf $SDK_DIR/dist node_modules/@metamask/sdk/dist
 cp -rf $SDK_DIR/package.json node_modules/@metamask/sdk/package.json
+
+cp -rf $SDK_ANALYTICS_DIR/dist node_modules/@metamask/sdk-analytics/dist
+cp -rf $SDK_ANALYTICS_DIR/node_modules node_modules/@metamask/sdk-analytics/node_modules
+cp -rf $SDK_ANALYTICS_DIR/package.json node_modules/@metamask/sdk-analytics/package.json
 
 echo "All done."
