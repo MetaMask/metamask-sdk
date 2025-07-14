9ed0e437c4a41922fce66d8efe507f67a8e6bcd8
Javier Ribó <javi.ribo@consensys.net>
2025-07-14 13:06:31 +0200

    fix: better manage StoreAdapterNode

--- packages/sdk-multichain/src/store/adapters/node.ts
+++ packages/sdk-multichain/src/store/adapters/node.ts
@@ -6,13 +6,25 @@ const CONFIG_FILE = path.resolve(process.cwd(), '.metamask.json');
 
 export class StoreAdapterNode extends StoreAdapter {
 
+
+  private safeParse(contents: string): Record<string, string> {
+    try {
+      return JSON.parse(contents);
+    } catch (e) {
+      return {};
+    }
+  }
+
   async getItem(key: string): Promise<string | null> {
     if (!fs.existsSync(CONFIG_FILE)) {
       return null;
     }
     const contents = fs.readFileSync(CONFIG_FILE, 'utf8');
-    const config = JSON.parse(contents);
-     return config[key] || null;
+    const config = this.safeParse(contents);
+    if (config[key] !== undefined) {
+      return config[key];
+    }
+    return null;
   }
 
   async setItem(key: string, value: string): Promise<void> {
@@ -20,7 +32,7 @@ export class StoreAdapterNode extends StoreAdapter {
       fs.writeFileSync(CONFIG_FILE, '{}');
     }
     const contents = fs.readFileSync(CONFIG_FILE, 'utf8');
-    const config = JSON.parse(contents);
+    const config = this.safeParse(contents);
     config[key] = value;
     fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
   }
@@ -30,7 +42,7 @@ export class StoreAdapterNode extends StoreAdapter {
       return;
     }
     const contents = fs.readFileSync(CONFIG_FILE, 'utf8');
-    const config = JSON.parse(contents);
+    const config = this.safeParse(contents);
     delete config[key];
     fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
   }
