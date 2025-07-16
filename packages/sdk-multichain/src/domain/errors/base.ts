bd96bd09fc35d201a042c896887f97edb9c0b821
Javier Ribó <javi.ribo@consensys.net>
2025-07-16 16:27:47 +0200

    fix: remove empty lines

--- packages/sdk-multichain/src/domain/errors/base.ts
+++ packages/sdk-multichain/src/domain/errors/base.ts
@@ -1,9 +1,5 @@
 import type { ErrorCodes } from "./types";
 
-
-
-
-
 export abstract class BaseErr<C extends string, T extends ErrorCodes> extends Error {
   constructor(
     public readonly message: `${C}Err${T}: ${string}`,
