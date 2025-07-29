daa87ee8741dff3fd212b454adcd06f358e6a2a7
Javier Ribó <javi.ribo@consensys.net>
2025-07-29 13:03:21 +0200

    fix: logger typo

--- packages/sdk-multichain/src/ui/node/otp.ts
+++ packages/sdk-multichain/src/ui/node/otp.ts
@@ -2,6 +2,10 @@ import { AbstractOTPCodeModal, createLogger, type OTPCodeWidgetProps } from '../
 
 const logger = createLogger('metamask-sdk:ui');
 
+/**
+ * TODO: This is a placeholder for the OTP code modal.
+ * It will be replaced with the actual OTP code modal once it is implemented.
+ */
 export class OTPCodeModal extends AbstractOTPCodeModal {
 	//TODO: Change HTMLMmInstallModalElement once otp modal is done
 	instance: HTMLMmInstallModalElement | undefined;
@@ -11,7 +15,7 @@ export class OTPCodeModal extends AbstractOTPCodeModal {
 			unmount: () => {},
 			sync: (otpValue: string) => {
 				if (otpValue !== '') {
-					logger(`[UI: pendingModal-nodejs: OTPCodeModal()] Choose the following value on your metamask mobile wallet: ${otpValue}`);
+					logger(`[UI: otpCodeModal-nodejs: OTPCodeModal()] Choose the following value on your metamask mobile wallet: ${otpValue}`);
 				}
 			},
 		};
