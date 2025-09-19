2cc064f8f5128b6e6dc3e84fc10d04c325a6dc07
Javier Ribó <javi.ribo@consensys.net>
2025-09-19 13:35:24 +0200

    fix: convert craco to cjs

--- playground/multichain-react/craco.config.js
+++ playground/multichain-react/craco.config.js
@@ -6,12 +6,12 @@ module.exports = {
 			webpackConfig.resolve.fallback = {
 				...webpackConfig.resolve.fallback,
 				buffer: require.resolve('buffer'),
-				process: require.resolve('process/browser'),
+				process: require.resolve('process/browser.js'),
 			};
 			webpackConfig.plugins.push(
 				new webpack.ProvidePlugin({
 					Buffer: ['buffer', 'Buffer'],
-					process: 'process/browser',
+					process: 'process/browser.js',
 				}),
 			);
 			return webpackConfig;
