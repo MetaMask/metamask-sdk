8e20265f685b482e1db8bcffa67e680eac716ed3
Alex Mendonca <alex.mendonca@consensys.net>
2025-10-08 13:49:15 +0100

    add format changelog scripts

---
+++ scripts/format-changelog.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# -eq 0 ]]; then
+  echo "Missing package name."
+  exit 1
+fi
+
+package_name="$1"
+shift  # remove package name from arguments
+
+# Get the script directory and find the root node_modules
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
+
+# Format/fix the changelog using prettier
+if [[ "${GITHUB_REF:-}" =~ '^release/' ]]; then
+  "${ROOT_DIR}/node_modules/.bin/auto-changelog" update --prettier --tag-prefix "${package_name}@" --rc "$@"
+else
+  "${ROOT_DIR}/node_modules/.bin/auto-changelog" update --prettier --tag-prefix "${package_name}@" "$@"
+fi
+
