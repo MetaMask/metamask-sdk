dc8ac1812ffa33ed4101a1f30e882c99244f9698
Alex Donesky <adonesky@gmail.com>
2025-10-15 11:52:24 -0500

    chore: remove redundant SDKVersion component

--- packages/sdk-multichain-ui/src/components/misc/SDKVersion.tsx
+++
@@ -1,7 +0,0 @@
-import { h } from "@stencil/core";
-
-export default function ConnectVersion({version}: {version?: string}) {
-  return (
-    <div class="version-title">Connect Version {version ? `v${version}`:`unknown`}</div>
-  )
-}
