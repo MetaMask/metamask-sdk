9a9d9bf45746db02bc19a3d68cb8b91c9eae064e
Alex Mendonca <alex.mendonca@consensys.net>
2025-10-21 23:28:04 +0100

    chore: update provider

--- packages/connect-evm/src/connect.ts
+++ packages/connect-evm/src/connect.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import {
   createMetamaskSDK,
   MultichainOptions,
@@ -6,83 +7,108 @@ import {
   RPCAPI,
   SessionData,
 } from '@metamask/multichain-sdk';
-import { EIP1193Provider } from './provider';
+import { EIP1193Provider, EIP155 } from './provider';
 import { getEthAccounts } from './utils/get-eth-accounts';
+import { Address } from './types';
 
 type CaipAccountId = `${string}:${string}:${string}`;
 
 type MinimalEventEmitter = Pick<EIP1193Provider, 'on' | 'off' | 'emit'>;
 
+type EventHandlers = {
+  accountsChanged: (accounts: string[]) => void;
+  chainChanged: (chainId: string) => void;
+};
+
 type MetamaskConnectEVMOptions = {
   core: MultichainCore;
   eventEmitter?: MinimalEventEmitter;
+  eventHandlers?: EventHandlers;
 };
 
-type Hex = `0x${string}`;
-
 export class MetamaskConnectEVM {
+  /** The core instance of the Multichain SDK */
   private core: MultichainCore;
 
-  currentChainId?: number;
+  /** The currently selected chain ID on the wallet */
+  private currentChainId?: number;
 
-  currentAccount?: string;
+  /** The currently selected account on the wallet */
+  private currentAccount?: Address;
 
+  accounts: Address[] = [];
+
+  /** An instance of the EIP-1193 provider interface */
   private provider: EIP1193Provider;
 
+  /** An event emitter for the EIP-1193 provider. Default to the provider instance*/
   private eventEmitter: MinimalEventEmitter;
 
+  /** The session scopes currently permitted */
   private sessionScopes: SessionData['sessionScopes'] = {};
 
-  constructor({ core, eventEmitter }: MetamaskConnectEVMOptions) {
+  /** Optional event handlers for the EIP-1193 provider events. */
+  private eventHandlers?: EventHandlers;
+
+  constructor({
+    core,
+    eventEmitter,
+    eventHandlers,
+  }: MetamaskConnectEVMOptions) {
     console.log('MetamaskConnectEVM constructor starts');
     this.core = core;
     this.provider = new EIP1193Provider(core);
-    this.eventEmitter = eventEmitter ?? this.provider;
+    this.eventEmitter = eventEmitter ?? {};
+    this.eventHandlers = eventHandlers;
 
     this.core.on('wallet_sessionChanged', (session) => {
       console.log('wallet_sessionChanged', session);
       this.sessionScopes = session?.sessionScopes ?? {};
+
       const ethAccounts = getEthAccounts(this.sessionScopes);
-
       this.currentAccount = ethAccounts[0];
-
-      if (!session) {
-        this.eventEmitter?.emit('disconnect');
-      }
+      this.accounts = ethAccounts;
     });
 
-    // @ts-expect-error - type mismatch
-    if (window.ethereum?.isMetaMask) {
-      // @ts-expect-error - type mismatch
-      window.ethereum.on('accountsChanged', (accounts: string[]) => {
-        console.log('accountsChanged', accounts);
-        this.currentAccount = accounts[0];
-      });
+    // Setup the event listener for the wallet's EIP-1193 accountsChanged event
+    window.addEventListener('message', (event: unknown) => {
+      if (
+        event?.data?.data?.name === 'metamask-provider' &&
+        event.origin === location.origin
+      ) {
+        const data = event?.data?.data?.data;
+        if (data?.method === 'metamask_chainChanged') {
+          const chainId = Number(data?.params.chainId);
+          console.log('metamask_chainChanged', { chainId });
+          this.currentChainId = chainId;
+          // TODO: (@wenfix): better setter
+          this.provider.currentChainId = chainId;
+          this.eventHandlers?.chainChanged?.(chainId);
+        }
 
-      // @ts-expect-error - type mismatch
-      window.ethereum.on('chainChanged', (chainId: Hex) => {
-        console.log('chainChanged', chainId);
-        this.currentChainId = Number(chainId);
-      });
-    }
+        if (data?.method === 'metamask_accountsChanged') {
+          const accounts = data?.params;
+          console.log('metamask_accountsChanged', { accounts });
+          this.currentAccount = accounts[0];
+          this.eventHandlers?.accountsChanged?.(accounts);
+        }
+      }
+    });
 
     console.log('MetamaskConnectEVM constructor finished');
   }
 
-  async connect({
-    chainId = 1,
-    account,
-  }: {
-    chainId: number;
-    account: string;
-  }) {
-    const caipChainId: Scope<RPCAPI>[] = chainId ? [`eip155:${chainId}`] : [];
-    const caipAccountId: CaipAccountId[] = account
-      ? [`eip155:${chainId}:${account}`]
+  async connect({ chainId, account }: { chainId: number; account?: string }) {
+    const caipChainId: Scope<RPCAPI>[] = chainId
+      ? [`${EIP155}:${chainId}`]
       : [];
+
+    const caipAccountId: CaipAccountId[] =
+      chainId && account ? [`${EIP155}:${chainId}:${account}`] : [];
 
     console.log('caipChainId', caipChainId);
     console.log('caipAccountId', caipAccountId);
+
     await this.core.connect(caipChainId, caipAccountId);
 
     this.currentChainId = chainId;
@@ -92,14 +118,17 @@ export class MetamaskConnectEVM {
       chainId: this.currentChainId,
     };
 
+    this.provider.emit('connect', result);
+
     return result;
   }
 
   async disconnect() {
-    console.log('disconnect starts');
-    const disconnected = await this.core.disconnect();
-    console.log('disconnected', disconnected);
-    console.log('disconnect ends');
+    await this.core.disconnect();
+
+    this.provider.emit('disconnect');
+
+    this.clearConnectionState();
   }
 
   async terminate() {
@@ -117,14 +146,33 @@ export class MetamaskConnectEVM {
   async getAccount(): Promise<string | undefined> {
     return this.currentAccount;
   }
+
+  switchChain({ chainId }: { chainId: number }) {
+    this.currentChainId = chainId;
+    this.provider.currentChainId = chainId;
+    this.provider.emit('chainChanged', { chainId });
+  }
+
+  private clearConnectionState() {
+    this.accounts = [];
+    this.currentAccount = undefined;
+    this.currentChainId = undefined;
+  }
 }
 
 export async function createEVMLayer(
-  options: MultichainOptions & { eventEmitter?: MinimalEventEmitter },
+  options: MultichainOptions & {
+    eventEmitter?: MinimalEventEmitter;
+    eventHandlers?: EventHandlers;
+  },
 ) {
   try {
     const core = await createMetamaskSDK(options);
-    return new MetamaskConnectEVM({ core, eventEmitter: options.eventEmitter });
+    return new MetamaskConnectEVM({
+      core,
+      eventEmitter: options.eventEmitter,
+      eventHandlers: options.eventHandlers,
+    });
   } catch (error) {
     console.error('Error creating core', error);
     throw error;
--- packages/connect-evm/src/provider.ts
+++ packages/connect-evm/src/provider.ts
@@ -14,11 +14,13 @@ type SDKEvents = {
  * Basic EIP-1193 Provider Implementation
  */
 export class EIP1193Provider extends EventEmitter<SDKEvents> {
-  private connectCore: MultichainCore;
+  private core: MultichainCore;
+
+  currentChainId?: number;
 
   constructor(core: MultichainCore) {
     super();
-    this.connectCore = core;
+    this.core = core;
   }
 
   request(request: {
@@ -26,8 +28,10 @@ export class EIP1193Provider extends EventEmitter<SDKEvents> {
     params: unknown;
     chainId?: string;
   }): Promise<unknown> {
-    return this.connectCore.invokeMethod({
-      scope: `${EIP155}:${request.chainId}`,
+    const _chainId = request.chainId ?? this.currentChainId;
+    // TODO (@wenfix): remap methods to wallet_createSession and wallet_getSession
+    return this.core.invokeMethod({
+      scope: `${EIP155}:${_chainId}`,
       request: {
         method: request.method,
         params: request.params,
