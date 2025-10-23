59a9183b8e9268fea45ec445f53c7965a9e032f1
Alex Mendonca <alex.mendonca@consensys.net>
2025-10-23 15:46:51 +0100

    basic feature set

--- packages/connect-evm/src/connect.ts
+++ packages/connect-evm/src/connect.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import {
   createMetamaskSDK,
   MultichainOptions,
@@ -18,6 +17,8 @@ type MinimalEventEmitter = Pick<EIP1193Provider, 'on' | 'off' | 'emit'>;
 type EventHandlers = {
   accountsChanged: (accounts: string[]) => void;
   chainChanged: (chainId: string) => void;
+  connect: (result: { accounts: string[]; chainId: number }) => void;
+  disconnect: () => void;
 };
 
 type MetamaskConnectEVMOptions = {
@@ -25,6 +26,23 @@ type MetamaskConnectEVMOptions = {
   eventEmitter?: MinimalEventEmitter;
   eventHandlers?: EventHandlers;
 };
+
+type AddEthereumChainParameter = {
+  chainId?: string;
+  chainName?: string;
+  nativeCurrency?: {
+    name?: string;
+    symbol?: string;
+    decimals?: number;
+  };
+  rpcUrls?: string[];
+  blockExplorerUrls?: string[];
+  iconUrls?: string[];
+};
+
+function toHex(value: number | string): string {
+  return `0x${value.toString(16)}`;
+}
 
 export class MetamaskConnectEVM {
   /** The core instance of the Multichain SDK */
@@ -41,61 +59,85 @@ export class MetamaskConnectEVM {
   /** An instance of the EIP-1193 provider interface */
   private provider: EIP1193Provider;
 
-  /** An event emitter for the EIP-1193 provider. Default to the provider instance*/
-  private eventEmitter: MinimalEventEmitter;
-
   /** The session scopes currently permitted */
   private sessionScopes: SessionData['sessionScopes'] = {};
 
   /** Optional event handlers for the EIP-1193 provider events. */
   private eventHandlers?: EventHandlers;
 
-  constructor({
-    core,
-    eventEmitter,
-    eventHandlers,
-  }: MetamaskConnectEVMOptions) {
+  /** The latest chain configuration received from a switchEthereumChain request */
+  private latestChainConfiguration?: AddEthereumChainParameter;
+
+  /** The handler for the metamask-provider events */
+  private metamaskProviderHandler: (event: MessageEvent) => void;
+
+  /** The handler for the wallet_sessionChanged event */
+  private sessionChangedHandler: (session: SessionData) => void;
+
+  constructor({ core, eventHandlers }: MetamaskConnectEVMOptions) {
     console.log('MetamaskConnectEVM constructor starts');
     this.core = core;
     this.provider = new EIP1193Provider(core);
-    this.eventEmitter = eventEmitter ?? {};
     this.eventHandlers = eventHandlers;
 
-    this.core.on('wallet_sessionChanged', (session) => {
-      console.log('wallet_sessionChanged', session);
-      this.sessionScopes = session?.sessionScopes ?? {};
+    // Setup the event listeners for the wallet's EIP-1193 events
+    this.setupEventListeners();
+  }
 
-      const ethAccounts = getEthAccounts(this.sessionScopes);
-      this.currentAccount = ethAccounts[0];
-      this.accounts = ethAccounts;
-    });
-
-    // Setup the event listener for the wallet's EIP-1193 accountsChanged event
-    window.addEventListener('message', (event: unknown) => {
+  private setupEventListeners() {
+    this.metamaskProviderHandler = (event: MessageEvent) => {
       if (
         event?.data?.data?.name === 'metamask-provider' &&
         event.origin === location.origin
       ) {
         const data = event?.data?.data?.data;
-        if (data?.method === 'metamask_chainChanged') {
-          const chainId = Number(data?.params.chainId);
-          console.log('metamask_chainChanged', { chainId });
-          this.currentChainId = chainId;
-          // TODO: (@wenfix): better setter
-          this.provider.currentChainId = chainId;
-          this.eventHandlers?.chainChanged?.(chainId);
-        }
 
+        console.log('message', data);
         if (data?.method === 'metamask_accountsChanged') {
           const accounts = data?.params;
           console.log('metamask_accountsChanged', { accounts });
           this.currentAccount = accounts[0];
           this.eventHandlers?.accountsChanged?.(accounts);
         }
-      }
-    });
 
-    console.log('MetamaskConnectEVM constructor finished');
+        if (data?.method === 'metamask_chainChanged') {
+          const chainId = Number(data?.params.chainId);
+          console.log('metamask_chainChanged', { chainId });
+          this.currentChainId = chainId;
+
+          // TODO: (@wenfix): better setter
+          this.provider.currentChainId = chainId;
+          this.provider.emit('chainChanged', { chainId });
+          this.eventHandlers?.chainChanged?.(chainId.toString());
+
+          // If the chainId is the same as the latest chain configuration,
+          // we managed to set the chain correctly and can clear the configuration.
+          if (this.latestChainConfiguration?.chainId === chainId.toString()) {
+            this.latestChainConfiguration = null;
+          }
+        }
+
+        if (data?.error?.code === 4902) {
+          console.log('metamask_switchEthereumChain error', data?.error);
+
+          this.addEthereumChain({ chainId: this.currentChainId });
+        }
+      }
+    };
+
+    this.sessionChangedHandler = (session: SessionData) => {
+      console.log('sessionChanged', session);
+      this.sessionScopes = session?.sessionScopes ?? {};
+
+      console.log('sessionScopes', this.sessionScopes);
+      const ethAccounts = getEthAccounts(this.sessionScopes);
+      this.currentAccount = ethAccounts[0];
+      this.accounts = ethAccounts;
+    };
+
+    window.addEventListener('message', this.metamaskProviderHandler);
+
+    this.core.on('wallet_sessionChanged', this.sessionChangedHandler);
   }
 
   async connect({ chainId, account }: { chainId: number; account?: string }) {
@@ -105,9 +147,6 @@ export class MetamaskConnectEVM {
 
     const caipAccountId: CaipAccountId[] =
       chainId && account ? [`${EIP155}:${chainId}:${account}`] : [];
-
-    console.log('caipChainId', caipChainId);
-    console.log('caipAccountId', caipAccountId);
 
     await this.core.connect(caipChainId, caipAccountId);
 
@@ -119,6 +158,7 @@ export class MetamaskConnectEVM {
     };
 
     this.provider.emit('connect', result);
+    this.eventHandlers?.accountsChanged?.(result.accounts);
 
     return result;
   }
@@ -127,10 +167,21 @@ export class MetamaskConnectEVM {
     await this.core.disconnect();
 
     this.provider.emit('disconnect');
+    this.eventHandlers?.accountsChanged?.([undefined]);
+    this.eventHandlers?.disconnect?.();
 
     this.clearConnectionState();
+
+    if (this.metamaskProviderHandler) {
+      window.removeEventListener('message', this.metamaskProviderHandler);
+    }
+
+    if (this.sessionChangedHandler) {
+      this.core.off('wallet_sessionChanged', this.sessionChangedHandler);
+    }
   }
 
+  // Legacy method for backward compatibility
   async terminate() {
     this.disconnect();
   }
@@ -147,16 +198,88 @@ export class MetamaskConnectEVM {
     return this.currentAccount;
   }
 
-  switchChain({ chainId }: { chainId: number }) {
-    this.currentChainId = chainId;
-    this.provider.currentChainId = chainId;
-    this.provider.emit('chainChanged', { chainId });
+  /**
+   * Switches the Ethereum chain. Will track state internally whenever possible.
+   *
+   * @param chainId - The chain ID to switch to
+   * @param chainConfiguration - The chain configuration to use in case the chain is not present by the wallet
+   */
+  switchChain({
+    chainId,
+    chainConfiguration,
+  }: {
+    chainId: number;
+    chainConfiguration?: AddEthereumChainParameter;
+  }) {
+    if (this.currentChainId === chainId) {
+      return;
+    }
+
+    // TODO: Check if approved scopes have the chain and early return
+
+    // Save the chain configuration in case it's not present in the wallet
+    this.latestChainConfiguration = chainConfiguration;
+
+    const hexChainId = toHex(chainId);
+
+    this.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: hexChainId }],
+    });
   }
 
+  /**
+   * Clears the internal connection state: accounts and chainId
+   */
   private clearConnectionState() {
     this.accounts = [];
     this.currentAccount = undefined;
     this.currentChainId = undefined;
+  }
+
+  /**
+   * Switches the Ethereum chain
+   * @param chainId - The chain ID to switch to
+   */
+  private async switchEthereumChain({ chainId }: { chainId: number }) {
+    const hexChainId = toHex(chainId);
+
+    await this.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: hexChainId }],
+    });
+  }
+
+  /**
+   * Adds an Ethereum chain using the latest chain configuration received from
+   * a switchEthereumChain request
+   */
+  private async addEthereumChain() {
+    if (!this.latestChainConfiguration) {
+      throw new Error('No chain configuration found.');
+    }
+
+    await this.request({
+      method: 'wallet_addEthereumChain',
+      params: [this.latestChainConfiguration],
+    });
+  }
+
+  /**
+   * Submits a request to the EIP-1193 provider
+   * @param request - The request object containing the method and params
+   */
+  private async request(request: { method: string; params: unknown[] }) {
+    await window.postMessage(
+      {
+        target: 'metamask-contentscript',
+        data: {
+          name: 'metamask-provider',
+          data: request,
+        },
+      },
+      location.origin,
+    );
   }
 }
 
--- packages/connect-evm/src/provider.ts
+++ packages/connect-evm/src/provider.ts
@@ -14,21 +14,28 @@ type SDKEvents = {
  * Basic EIP-1193 Provider Implementation
  */
 export class EIP1193Provider extends EventEmitter<SDKEvents> {
+  /** The core instance of the Multichain SDK */
   private core: MultichainCore;
 
-  currentChainId?: number;
+  /** The currently selected chain ID on the wallet */
+  public currentChainId?: number;
 
   constructor(core: MultichainCore) {
     super();
     this.core = core;
   }
 
+  /**
+   * Submits a request to the EIP-1193 provider
+   * @param request - The request object containing the method and params
+   */
   request(request: {
     method: string;
     params: unknown;
     chainId?: string;
   }): Promise<unknown> {
     const _chainId = request.chainId ?? this.currentChainId;
+
     // TODO (@wenfix): remap methods to wallet_createSession and wallet_getSession
     return this.core.invokeMethod({
       scope: `${EIP155}:${_chainId}`,
